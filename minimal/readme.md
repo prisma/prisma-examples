9ed31bfdfe34f1f25efd3ac3ac00e56a8e42e018
timsuchanek <tim.suchanek@gmail.com>
2018-08-18 13:00:47 +0200

    move & update example

---
+++ minimal/readme.md
@@ -0,0 +1,10 @@
+# minimal example
+
+## Usage
+
+```
+yarn prisma generate
+yarn start
+```
+
+The main file of this example is `src/usage.ts`.
--- minimal/src/usage.ts
+++ minimal/src/usage.ts
@@ -2,7 +2,7 @@ import { Prisma } from './prisma'
 ;(async () => {
   const prisma = new Prisma()
 
-  const result = await prisma.delegate.query.cities<any>()
+  const result = await prisma.query.cats()
 
   console.log(result)
 })()
