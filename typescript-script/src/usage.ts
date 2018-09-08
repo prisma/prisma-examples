d7844fbc512f1a967063b9de5be6f56efa3c29dd
Abhi Aiyer <abhiaiyer91@gmail.com>
2018-09-08 07:58:47 -0700

    fix(nodets): node script and ts script examples

--- typescript-script/src/usage.ts
+++ typescript-script/src/usage.ts
@@ -1,11 +1,11 @@
-import { prisma } from '../generated'
+import { prisma } from './generated'
 
 ;(async () => {
 
   const result = await prisma
     .cat({ id: 'cjky74byofp1n0b07ux4038oe' })
     .favBrother()
-  
+
   console.log(result)
 
   await prisma.createMaster({
@@ -18,5 +18,5 @@ import { prisma } from '../generated'
       ],
     },
   })
-  
+
 })()
