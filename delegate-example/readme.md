68110eb5eeee0084a19c3c89e79a09cd2ca007fb
Abhi Aiyer <abhiaiyer91@gmail.com>
2018-09-09 19:56:59 -0700

    Add schema delegation examples

--- delegate-example/readme.md
+++
@@ -1,3 +0,0 @@
-# orm-tutorial
-
-This is the end product of [this Prisma ORM Tutorial](https://github.com/timsuchanek/orm-tutorial/tree/master)
---
+++ node-graphql-schema-delegation/src/index.js
@@ -0,0 +1,13 @@
+import { GraphQLServer } from 'graphql-yoga'
+import { resolvers } from './resolvers'
+import { Prisma } from './generated'
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: {
+    db: new Prisma({ debug: true }),
+  },
+})
+
+server.start(() => console.log('Server is running on localhost:4000'))
