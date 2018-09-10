3509a1d12fa45976c2a342096d06b9e7306627f9
Nikolas Burk <nikolas.burk@gmail.com>
2018-09-10 19:52:15 +0200

    fix schema path

--- node-graphql-auth/src/index.js
+++ node-graphql-auth/src/index.js
@@ -3,7 +3,7 @@ const { resolvers } = require("./resolvers");
 const { Prisma } = require("./generated/prisma");
 
 const server = new GraphQLServer({
-  typeDefs: "./schema/schema.graphql",
+  typeDefs: "src/schema/schema.graphql",
   resolvers,
   context: req => {
     return {
