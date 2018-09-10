4e656a599e5617b521dff08982f8bbd1fdf7e195
timsuchanek <tim.suchanek@gmail.com>
2018-09-10 17:04:36 +0200

    add flow-script

---
+++ flow-script/src/usage.js
@@ -0,0 +1,23 @@
+/**
+ * @flow
+ */
+import { prisma } from './generated'
+
+;(async () => {
+  const result = await prisma
+    .cat({ id: 'cjky74byofp1n0b07ux4038oe' })
+    .favBrother()
+
+  console.log(result)
+
+  // await prisma.createMaster({
+  //   catz: {
+  //     create: {
+  //       color: 'red',
+  //       name: 'Bob',
+  //     },
+  //   },
+  // })
+
+  // prisma.
+})()
