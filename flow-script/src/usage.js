3c65df7ed14a171103bebe26e4a10aaaad3a8e4d
Johannes Schickling <schickling.j@gmail.com>
2018-09-12 11:56:21 +0200

    Update usage.js

--- flow-script/src/usage.js
+++ flow-script/src/usage.js
@@ -5,7 +5,6 @@ import { prisma } from './generated'
 
 // A `main` function so that we can use async/await
 async function main() {
-  
   const result = await prisma
     .cat({ id: 'cjky74byofp1n0b07ux4038oe' })
     .favBrother()
