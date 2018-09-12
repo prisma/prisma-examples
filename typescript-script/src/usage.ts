5436353386f4909be79080fbbf9997db2ac31ad1
Johannes Schickling <schickling.j@gmail.com>
2018-09-12 11:57:40 +0200

    Update usage.ts

--- typescript-script/src/usage.ts
+++ typescript-script/src/usage.ts
@@ -1,5 +1,8 @@
 import { prisma } from './generated'
-;(async () => {
+
+// A `main` function so that we can use async/await
+async function main() {
+  
   const result = await prisma.cats(
     {},
     `fragment Cat on Cat { id name favBrother { id } }`,
@@ -17,4 +20,7 @@ import { prisma } from './generated'
       ],
     },
   })
-})()
+  
+}
+
+main().catch(e => console.error(e))
