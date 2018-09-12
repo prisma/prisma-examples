bf3f126b438ead7f4034f8a3ec16b4ae0f47c546
Johannes Schickling <schickling.j@gmail.com>
2018-09-12 11:56:11 +0200

    Update usage.js

--- flow-script/src/usage.js
+++ flow-script/src/usage.js
@@ -3,21 +3,14 @@
  */
 import { prisma } from './generated'
 
-;(async () => {
+// A `main` function so that we can use async/await
+async function main() {
+  
   const result = await prisma
     .cat({ id: 'cjky74byofp1n0b07ux4038oe' })
     .favBrother()
 
   console.log(result)
+}
 
-  // await prisma.createMaster({
-  //   catz: {
-  //     create: {
-  //       color: 'red',
-  //       name: 'Bob',
-  //     },
-  //   },
-  // })
-
-  // prisma.
-})()
+main().catch(e => console.error(e))
