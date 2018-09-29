8511ad66826d3e92a6c685783cd2a6a4dc6470c5
Abhi Aiyer <abhiaiyer91@gmail.com>
2018-09-29 10:47:29 -0700

    WIP

--- node-graphql-subscriptions/src/resolvers/User.js
+++ node-graphql-subscriptions/src/resolvers/User.js
@@ -1,7 +1,4 @@
 const User = {
-  id: parent => parent.id,
-  email: parent => parent.email,
-  name: parent => parent.name,
   posts: (parent, args, ctx) => ctx.db.user({ id: parent.id }).posts(),
 }
 
