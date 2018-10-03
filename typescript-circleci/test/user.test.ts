e5babd2132cda5b6868fc0d133f1e10fb9b4571b
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-10-04 06:03:11 +1000

    Use datamodel from other examples

--- typescript-circleci/test/user.test.ts
+++ typescript-circleci/test/user.test.ts
@@ -3,5 +3,5 @@ import { prisma } from '../generated/prisma-client'
 test('Resolve users', async () => {
     const users = await prisma.users()
     expect(users.length).toBe(1)
-    expect(users[0].name).toBe('Example')
+    expect(users[0].name).toBe('Alice')
 })
