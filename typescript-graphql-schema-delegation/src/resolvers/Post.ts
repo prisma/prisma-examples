773c80079ab4475f5935eec4d003354149a0786b
Abhi Aiyer <abhiaiyer91@gmail.com>
2018-10-10 19:11:17 -0700

    Remove resolver codegen from delegation example (#149)

--- typescript-graphql-schema-delegation/src/resolvers/Post.ts
+++ typescript-graphql-schema-delegation/src/resolvers/Post.ts
@@ -1,18 +1,4 @@
-import { PostResolvers } from '../generated/resolvers'
-import { TypeMap } from './types/TypeMap'
-import { UserParent } from './User'
-
-export interface PostParent {
-  id: string
-  createdAt: string
-  updatedAt: string
-  isPublished: boolean
-  title: string
-  content: string
-  author: UserParent
-}
-
-export const Post: PostResolvers.Type<TypeMap> = {
+export const Post = {
   id: parent => parent.id,
   createdAt: parent => parent.createdAt,
   updatedAt: parent => parent.updatedAt,
--- typescript-graphql-schema-delegation/src/resolvers/index.ts
+++ typescript-graphql-schema-delegation/src/resolvers/index.ts
@@ -1,12 +1,9 @@
-import { IResolvers } from '../generated/resolvers'
-import { TypeMap } from './types/TypeMap'
-
 import { Query } from './Query'
 import { Mutation } from './Mutation'
 import { Post } from './Post'
 import { User } from './User'
 
-export const resolvers: IResolvers<TypeMap> = {
+export const resolvers = {
   Query,
   Mutation,
   Post,
