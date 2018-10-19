5f56781d4c51708f4552c5eb0b18040684af2aa3
schickling <schickling.j@gmail.com>
2018-10-19 13:03:00 +0200

    fix arg names

--- typescript-graphql/src/resolvers/Post.ts
+++ typescript-graphql/src/resolvers/Post.ts
@@ -16,5 +16,5 @@ export const Post: PostResolvers.Type = {
   isPublished: parent => parent.isPublished,
   title: parent => parent.title,
   content: parent => parent.content,
-  author: (parent, _args, ctx) => ctx.db.post({ id: parent.id }).author(),
+  author: (parent, args, ctx) => ctx.db.post({ id: parent.id }).author(),
 }
--- typescript-graphql/src/resolvers/User.ts
+++ typescript-graphql/src/resolvers/User.ts
@@ -13,6 +13,6 @@ export const User: UserResolvers.Type = {
   id: parent => parent.id,
   email: parent => parent.email,
   name: parent => parent.name,
-  posts: (parent, _args, ctx) => ctx.db.user({ id: parent.id }).posts(),
+  posts: (parent, args, ctx) => ctx.db.user({ id: parent.id }).posts(),
   createdAt: parent => parent.createdAt,
 }
