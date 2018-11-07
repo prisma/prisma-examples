13d54ae922a44a86bd8343e0458eaf06c2d7774b
schickling <schickling.j@gmail.com>
2018-11-07 13:25:12 -0800

    updated prisma example

--- typescript-graphql/src/generated/tmp-resolvers/index.ts
+++ typescript-graphql/src/generated/tmp-resolvers/index.ts
@@ -4,13 +4,13 @@
 import { Resolvers } from '../graphqlgen'
 
 import { Query } from './Query'
-import { Mutation } from './Mutation'
 import { Post } from './Post'
 import { User } from './User'
+import { Mutation } from './Mutation'
 
 export const resolvers: Resolvers = {
   Query,
-  Mutation,
   Post,
   User,
+  Mutation,
 }
