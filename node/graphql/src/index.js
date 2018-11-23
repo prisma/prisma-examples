05712f765efaf204d3696356c1e999c2aeafb7fe
Nikolas Burk <nikolas.burk@gmail.com>
2018-11-23 18:09:15 +0100

    improve schema

--- node/graphql/src/index.js
+++ node/graphql/src/index.js
@@ -6,8 +6,16 @@ const resolvers = {
     feed: (parent, args, context) => {
       return context.db.posts({ where: { published: true } })
     },
-    drafts: (parent, args, context) => {
-      return context.db.posts({ where: { published: false } })
+    filterPosts: (parent, { searchString }, context) => {
+      return context.db.posts({
+        where: {
+          OR: [{
+            title_contains: searchString
+          }, {
+            content_contains: searchString
+          }]
+        }
+      })
     },
     post: (parent, { id }, context) => {
       return context.db.post({ id })
