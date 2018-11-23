ace845383ee92779760fcf46474767f79a18daa8
Nikolas Burk <nikolas.burk@gmail.com>
2018-11-23 18:02:19 +0100

    cleanup graphql example

--- node/graphql/src/index.js
+++ node/graphql/src/index.js
@@ -2,6 +2,54 @@ const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
 const { resolvers } = require('./resolvers')
 
+const resolvers = {
+  Query: {
+    feed: (parent, args, context) => {
+      return context.db.posts({ where: { published: true } })
+    },
+    drafts: (parent, args, context) => {
+      return context.db.posts({ where: { published: false } })
+    },
+    post: (parent, { id }, context) => {
+      return context.db.post({ id })
+    },
+  },
+  Mutation: {
+    signupUser: (parent, { name, email }, context) => {
+      return context.db.createUser({
+        name,
+        email,
+      })
+    },
+    createDraft: (parent, { title, content, authorEmail }, context) => {
+      return context.db.createPost({
+        title,
+        content,
+        author: { connect: { email: authorEmail } },
+      })
+    },
+    publish: (parent, { id }, context) => {
+      return context.db.updatePost({
+        where: { id },
+        data: { published: true },
+      })
+    },
+    deletePost: (parent, { id }, context) => {
+      return context.db.deletePost({ id })
+    },
+  },
+  Post: {
+    author: (parent, args, context) => {
+      return context.db.post({ id: parent.id }).author()
+    },
+  },
+  User: {
+    posts: (parent, args, context) => {
+      return context.db.user({ id: parent.id }).posts()
+    },
+  },
+}
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
@@ -10,4 +58,4 @@ const server = new GraphQLServer({
   },
 })
 
-server.start(() => console.log('Server is running on localhost:4000'))
+server.start(() => console.log('Server is running on http://localhost:4000'))
