73fd5a3255ca54453689b39150885e66d5b3f5cd
Nikolas Burk <nikolas.burk@gmail.com>
2018-11-23 19:02:26 +0100

    cleanup subscriptions example

--- node/graphql-subscriptions/src/index.js
+++ node/graphql-subscriptions/src/index.js
@@ -1,6 +1,51 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
-const { resolvers } = require('./resolvers')
+
+const resolvers = {
+  Query: {
+    feed: (parent, args, context) => context.db.posts({ where: { published: true } }),
+    drafts: (parent, args, context) =>
+      context.db.posts({ where: { published: false } }),
+    post: (parent, { id }, context) => context.db.post({ id }),
+  },
+  Mutation: {
+    createDraft: (parent, { title, content }, context) => {
+      return context.db.createPost({
+        title,
+        content,
+      })
+    },
+    deletePost: (parent, { id }, context) => context.db.deletePost({ id }),
+    publish: (parent, { id }, context) => {
+      return context.db.updatePost({
+        where: { id },
+        data: { published: true },
+      })
+    },
+  },
+  Subscription: {
+    posts: {
+      subscribe: async (parent, args, context) => {
+        return context.db.$subscribe
+          .post({
+            where: {
+              mutation_in: ['CREATED', 'UPDATED'],
+            },
+          })
+          .node()
+      },
+      resolve: payload => {
+        return payload
+      },
+    },
+  },
+  Post: {
+    author: (parent, args, context) => context.db.post({ id: parent.id }).author(),
+  },
+  User: {
+    posts: (parent, args, context) => context.db.user({ id: parent.id }).posts(),
+  }
+}
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
