959530393e5baf3db5b819af917961287599d91b
Nikolas Burk <nikolas.burk@gmail.com>
2018-11-26 11:43:35 +0100

    validate everything works

--- node/graphql-schema-delegation/src/prisma-binding.js
+++
@@ -1,8 +0,0 @@
-const { Prisma } = require('prisma-binding')
-
-module.exports = {
-  prisma: new Prisma({
-    typeDefs: './generated/prisma.graphql',
-    endpoint: '__PRISMA_ENDPOINT__',
-  }),
-}
