640a22c26f5eaa396c74f2b6a644a1014a7074bd
Nikolas Burk <nikolas.burk@gmail.com>
2018-11-26 13:45:56 +0100

    remove dangling endpoints

--- node/script/src/generated/prisma-client/index.js
+++ node/script/src/generated/prisma-client/index.js
@@ -16,7 +16,7 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://eu1.prisma.sh/nikolas/prisma/dev`
+  endpoint: ``
 });
 exports.prisma = new exports.Prisma();
 var models = [
