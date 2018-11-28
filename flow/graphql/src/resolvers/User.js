c87c5ce3182c0c4ba00d879c403def8116e86982
Nikolas Burk <nikolas.burk@gmail.com>
2018-11-28 14:43:52 +0100

    fix graphql example

--- flow/graphql/src/resolvers/User.js
+++ flow/graphql/src/resolvers/User.js
@@ -1,20 +1,11 @@
 /* @flow */
-import type { User_Type } from '../generated/resolvers'
-import type { TypeMap } from './types/TypeMap'
-import type { PostParent } from './Post'
+import { User_defaultResolvers } from '../generated/graphqlgen'
+import type { User_Resolvers } from '../generated/graphqlgen'
 
-export interface UserParent {
-  id: string;
-  email: string;
-  name: string;
-  posts: PostParent[];
-  createdAt: string;
-}
+export const User: User_Resolvers = {
+  ...User_defaultResolvers,
 
-export const User: User_Type<TypeMap> = {
-  id: parent => parent.id,
-  email: parent => parent.email,
-  name: parent => parent.name,
-  posts: (parent, args, ctx) => ctx.prisma.user({ id: parent.id }).posts(),
-  createdAt: parent => parent.createdAt,
+  posts: ({ id }, args, ctx, info) => {
+    return ctx.prisma.user({ id }).posts()
+  },
 }
