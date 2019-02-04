a7ef6cb38e97b413e76f06a1c65879713029aca7
Nikolas Burk <nikolas.burk@gmail.com>
2019-02-04 11:06:48 +0100

    prettier

--- node/script/prisma/seed.js
+++ node/script/prisma/seed.js
@@ -2,29 +2,32 @@ const { prisma } = require('../src/generated/prisma-client')
 
 async function main() {
   await prisma.createUser({
-    email: "alice@prisma.io",
-    name: "Alice",
+    email: 'alice@prisma.io',
+    name: 'Alice',
     posts: {
       create: {
-        title: "Join us for GraphQL Conf 2019 in Berlin",
-        content: "https://www.graphqlconf.org/",
-        published: true
-      }
-    }
+        title: 'Join us for GraphQL Conf 2019 in Berlin',
+        content: 'https://www.graphqlconf.org/',
+        published: true,
+      },
+    },
   })
   await prisma.createUser({
-    email: "bob@prisma.io",
-    name: "Bob",
+    email: 'bob@prisma.io',
+    name: 'Bob',
     posts: {
-      create: [{
-        title: "Subscribe to GraphQL Weekly for community news",
-        content: "https://graphqlweekly.com/",
-        published: true
-      }, {
-        title: "Follow Prisma on Twitter",
-        content: "https://twitter.com/prisma",
-      }]
-    }
+      create: [
+        {
+          title: 'Subscribe to GraphQL Weekly for community news',
+          content: 'https://graphqlweekly.com/',
+          published: true,
+        },
+        {
+          title: 'Follow Prisma on Twitter',
+          content: 'https://twitter.com/prisma',
+        },
+      ],
+    },
   })
 }
 
