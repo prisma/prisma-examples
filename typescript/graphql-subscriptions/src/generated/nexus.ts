458b6b803a15db5704814e697103637b0eca3721
Divyendu Singh <singh@prisma.io>
2019-05-13 10:34:14 +0200

    use prettier

--- typescript/graphql-subscriptions/src/generated/nexus.ts
+++ typescript/graphql-subscriptions/src/generated/nexus.ts
@@ -3,113 +3,132 @@
  * Do not make changes to this file directly
  */
 
-import * as types from "../types"
-
+import * as types from '../types'
 
 declare global {
   interface NexusGen extends NexusGenTypes {}
 }
 
-export interface NexusGenInputs {
-}
+export interface NexusGenInputs {}
 
-export interface NexusGenEnums {
-}
+export interface NexusGenEnums {}
 
 export interface NexusGenRootTypes {
-  Mutation: {};
-  Post: { // root type
-    content?: string | null; // String
-    createdAt: any; // DateTime!
-    id: string; // ID!
-    published: boolean; // Boolean!
-    title: string; // String!
-    updatedAt: any; // DateTime!
+  Mutation: {}
+  Post: {
+    // root type
+    content?: string | null // String
+    createdAt: any // DateTime!
+    id: string // ID!
+    published: boolean // Boolean!
+    title: string // String!
+    updatedAt: any // DateTime!
   }
-  Query: {};
-  Subscription: {};
-  String: string;
-  Int: number;
-  Float: number;
-  Boolean: boolean;
-  ID: string;
-  DateTime: any;
+  Query: {}
+  Subscription: {}
+  String: string
+  Int: number
+  Float: number
+  Boolean: boolean
+  ID: string
+  DateTime: any
 }
 
-export interface NexusGenAllTypes extends NexusGenRootTypes {
-}
+export interface NexusGenAllTypes extends NexusGenRootTypes {}
 
 export interface NexusGenFieldTypes {
-  Mutation: { // field return type
-    createDraft: NexusGenRootTypes['Post'] | null; // Post
-    deletePost: NexusGenRootTypes['Post'] | null; // Post
-    publish: NexusGenRootTypes['Post'] | null; // Post
+  Mutation: {
+    // field return type
+    createDraft: NexusGenRootTypes['Post'] | null // Post
+    deletePost: NexusGenRootTypes['Post'] | null // Post
+    publish: NexusGenRootTypes['Post'] | null // Post
   }
-  Post: { // field return type
-    content: string | null; // String
-    createdAt: any; // DateTime!
-    id: string; // ID!
-    published: boolean; // Boolean!
-    title: string; // String!
-    updatedAt: any; // DateTime!
+  Post: {
+    // field return type
+    content: string | null // String
+    createdAt: any // DateTime!
+    id: string // ID!
+    published: boolean // Boolean!
+    title: string // String!
+    updatedAt: any // DateTime!
   }
-  Query: { // field return type
-    feed: NexusGenRootTypes['Post'][] | null; // [Post!]
+  Query: {
+    // field return type
+    feed: NexusGenRootTypes['Post'][] | null // [Post!]
   }
-  Subscription: { // field return type
-    posts: NexusGenRootTypes['Post'] | null; // Post
+  Subscription: {
+    // field return type
+    posts: NexusGenRootTypes['Post'] | null // Post
   }
 }
 
 export interface NexusGenArgTypes {
   Mutation: {
-    createDraft: { // args
-      content?: string | null; // String
-      title?: string | null; // String
+    createDraft: {
+      // args
+      content?: string | null // String
+      title?: string | null // String
     }
-    deletePost: { // args
-      id?: string | null; // ID
+    deletePost: {
+      // args
+      id?: string | null // ID
     }
-    publish: { // args
-      id?: string | null; // ID
+    publish: {
+      // args
+      id?: string | null // ID
     }
   }
 }
 
-export interface NexusGenAbstractResolveReturnTypes {
-}
+export interface NexusGenAbstractResolveReturnTypes {}
 
 export interface NexusGenInheritedFields {}
 
-export type NexusGenObjectNames = "Mutation" | "Post" | "Query" | "Subscription";
+export type NexusGenObjectNames = 'Mutation' | 'Post' | 'Query' | 'Subscription'
 
-export type NexusGenInputNames = never;
+export type NexusGenInputNames = never
 
-export type NexusGenEnumNames = never;
+export type NexusGenEnumNames = never
 
-export type NexusGenInterfaceNames = never;
+export type NexusGenInterfaceNames = never
 
-export type NexusGenScalarNames = "Boolean" | "DateTime" | "Float" | "ID" | "Int" | "String";
+export type NexusGenScalarNames =
+  | 'Boolean'
+  | 'DateTime'
+  | 'Float'
+  | 'ID'
+  | 'Int'
+  | 'String'
 
-export type NexusGenUnionNames = never;
+export type NexusGenUnionNames = never
 
 export interface NexusGenTypes {
-  context: types.Context;
-  inputTypes: NexusGenInputs;
-  rootTypes: NexusGenRootTypes;
-  argTypes: NexusGenArgTypes;
-  fieldTypes: NexusGenFieldTypes;
-  allTypes: NexusGenAllTypes;
-  inheritedFields: NexusGenInheritedFields;
-  objectNames: NexusGenObjectNames;
-  inputNames: NexusGenInputNames;
-  enumNames: NexusGenEnumNames;
-  interfaceNames: NexusGenInterfaceNames;
-  scalarNames: NexusGenScalarNames;
-  unionNames: NexusGenUnionNames;
-  allInputTypes: NexusGenTypes['inputNames'] | NexusGenTypes['enumNames'] | NexusGenTypes['scalarNames'];
-  allOutputTypes: NexusGenTypes['objectNames'] | NexusGenTypes['enumNames'] | NexusGenTypes['unionNames'] | NexusGenTypes['interfaceNames'] | NexusGenTypes['scalarNames'];
-  allNamedTypes: NexusGenTypes['allInputTypes'] | NexusGenTypes['allOutputTypes']
-  abstractTypes: NexusGenTypes['interfaceNames'] | NexusGenTypes['unionNames'];
-  abstractResolveReturn: NexusGenAbstractResolveReturnTypes;
+  context: types.Context
+  inputTypes: NexusGenInputs
+  rootTypes: NexusGenRootTypes
+  argTypes: NexusGenArgTypes
+  fieldTypes: NexusGenFieldTypes
+  allTypes: NexusGenAllTypes
+  inheritedFields: NexusGenInheritedFields
+  objectNames: NexusGenObjectNames
+  inputNames: NexusGenInputNames
+  enumNames: NexusGenEnumNames
+  interfaceNames: NexusGenInterfaceNames
+  scalarNames: NexusGenScalarNames
+  unionNames: NexusGenUnionNames
+  allInputTypes:
+    | NexusGenTypes['inputNames']
+    | NexusGenTypes['enumNames']
+    | NexusGenTypes['scalarNames']
+  allOutputTypes:
+    | NexusGenTypes['objectNames']
+    | NexusGenTypes['enumNames']
+    | NexusGenTypes['unionNames']
+    | NexusGenTypes['interfaceNames']
+    | NexusGenTypes['scalarNames']
+  allNamedTypes:
+    | NexusGenTypes['allInputTypes']
+    | NexusGenTypes['allOutputTypes']
+  abstractTypes: NexusGenTypes['interfaceNames'] | NexusGenTypes['unionNames']
+  abstractResolveReturn: NexusGenAbstractResolveReturnTypes
 }
