b5ab7ad86ebf3df3726760f6118c0936cb8ff18b
Nikolas Burk <nikolas.burk@gmail.com>
2019-09-19 16:39:02 +0200

    remove photon.connect() call

--- deployment-platforms/codesandbox/src/script.ts
+++ deployment-platforms/codesandbox/src/script.ts
@@ -3,8 +3,6 @@ import { Photon } from "@generated/photon";
 const main = async () => {
   const photon = new Photon();
 
-  await photon.connect();
-
   // Get a list of all esisting users (Created already using a seed script)
   const users = await photon.users.findMany({});
   console.log(users);
