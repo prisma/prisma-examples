bcd374db15baeb0c4e298b13e127a3f9f447cf6e
Jason Kuhrt <jasonkuhrt@me.com>
2019-10-17 16:04:37 +0200

    js gql auth

--- javascript/graphql-auth/src/index.js
+++ javascript/graphql-auth/src/index.js
@@ -1,26 +1,17 @@
 const { GraphQLServer } = require('graphql-yoga')
-const { Photon } = require('@generated/photon')
-const { resolvers } = require('./resolvers')
-const { permissions } = require('./permissions')
 const { nexusPrismaPlugin } = require('nexus-prisma')
 const { makeSchema } = require('nexus')
-const { join } = require('path')
-const allTypes = require('./resolvers')
+const { Photon } = require('@generated/photon')
+const { permissions } = require('./permissions')
+const types = require('./types')
 
 const photon = new Photon()
 
-const nexusPrismaTypes = nexusPrismaPlugin({
-  types: allTypes,
-})
-const schema = makeSchema({
-  types: [allTypes, nexusPrismaTypes],
-  outputs: {
-    schema: join(__dirname, '/schema.graphql'),
-  },
-})
-
-const server = new GraphQLServer({
-  schema,
+new GraphQLServer({
+  schema: makeSchema({
+    types,
+    plugins: [nexusPrismaPlugin()],
+  }),
   middlewares: [permissions],
   context: request => {
     return {
@@ -28,10 +19,7 @@ const server = new GraphQLServer({
       photon,
     }
   },
-})
-
-
-server.start(() =>
+}).start(() =>
   console.log(
     `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/js/graphql-auth#5-using-the-graphql-api`,
   ),
