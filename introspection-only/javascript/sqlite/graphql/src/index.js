925d13e1153a19f8219e81bcca7c1d8e7709a1ed
Nikolas Burk <nikolas.burk@gmail.com>
2020-01-17 15:08:47 +0100

    adjust application code in GraphQL examples

--- introspection-only/javascript/sqlite/graphql/src/index.js
+++ introspection-only/javascript/sqlite/graphql/src/index.js
@@ -19,8 +19,6 @@ const Post = objectType({
   name: 'Post',
   definition(t) {
     t.model.id()
-    t.model.createdAt()
-    t.model.updatedAt()
     t.model.title()
     t.model.content()
     t.model.published()
