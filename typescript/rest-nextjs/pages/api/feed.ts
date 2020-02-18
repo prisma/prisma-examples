e721f0a8c0c674ee65c311f8da3f49f175a57bdc
Nikolas Burk <nikolas.burk@gmail.com>
2020-02-18 16:18:37 +0100

    include author in API call

--- typescript/rest-nextjs/pages/api/feed.ts
+++ typescript/rest-nextjs/pages/api/feed.ts
@@ -3,6 +3,9 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function handle(req, res) {
-  const posts = await prisma.post.findMany({ where: { published: true } })
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: { author: true },
+  })
   res.json(posts)
 }
