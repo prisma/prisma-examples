71d19bf34e8430e2e7176dc87050bccdee522587
Luca Steeb <contact@luca-steeb.com>
2020-03-03 16:55:40 +0700

    ci(github): update prisma deps, create an alpha branch
    This pauses renovate updates for Prisma dependencies, introduces auto ugprade
    mechanisms for master und alpha branches, and syncs the master branch into alpha.

---
+++ .github/scripts/sync-alpha.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -eu
+
+mkdir -p ~/.ssh
+echo "$SSH_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+git config --global user.email "prismabots@gmail.com"
+git config --global user.name "Prismo"
+
+git remote add github "git@github.com:$GITHUB_REPOSITORY.git"
+
+sh .github/scripts/ugrade-all.sh alpha
+
+git commit -am "chore(packages): bump prisma2 to $v"
+
+# fail silently if the unlikely event happens that this change already has been pushed either manually
+# or by an overlapping upgrade action
+git pull github "${GITHUB_REF}" --rebase || true
+
+# force-push to alpha
+git push github HEAD:"refs/heads/alpha" --force
