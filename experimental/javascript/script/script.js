29d5e8927e1bf388cb406e931af953e118a94a77
Daniel Norman <daniel@2color.me>
2020-03-12 16:52:36 +0100

    Update dependencies of experimental/script

--- experimental/javascript/script/script.js
+++ experimental/javascript/script/script.js
@@ -1,10 +1,11 @@
-const { Photon } = require('@prisma/photon')
-const photon = new Photon()
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
 
 // A `main` function so that we can use async/await
 async function main() {
   // Seed the database with users and posts
-  const user1 = await photon.users.create({
+  const user1 = await prisma.user.create({
     data: {
       email: 'alice@prisma.io',
       name: 'Alice',
@@ -20,7 +21,7 @@ async function main() {
       posts: true,
     },
   })
-  const user2 = await photon.users.create({
+  const user2 = await prisma.user.create({
     data: {
       email: 'bob@prisma.io',
       name: 'Bob',
@@ -45,13 +46,13 @@ async function main() {
   })
   console.log(`Created users: ${user1.name} (${user1.posts.length} post) and (${user2.posts.length} posts) `)
   // Retrieve all published posts
-  const allPosts = await photon.posts.findMany({
+  const allPosts = await prisma.post.findMany({
     where: { published: true },
   })
   console.log(`Retrieved all published posts: `, allPosts)
 
   // Create a new post (written by an already existing user with email alice@prisma.io)
-  const newPost = await photon.posts.create({
+  const newPost = await prisma.post.create({
     data: {
       title: 'Join the Prisma Slack community',
       content: 'http://slack.prisma.io',
@@ -66,7 +67,7 @@ async function main() {
   console.log(`Created a new post: `, newPost)
 
   // Publish the new post
-  const updatedPost = await photon.posts.update({
+  const updatedPost = await prisma.post.update({
     where: {
       id: newPost.id,
     },
@@ -77,7 +78,7 @@ async function main() {
   console.log(`Published the newly created post: `, updatedPost)
 
   // Retrieve all posts by user with email alice@prisma.io
-  const postsByUser = await photon.users
+  const postsByUser = await prisma.user
     .findOne({
       where: {
         email: 'alice@prisma.io',
@@ -92,5 +93,5 @@ main()
     throw e
   })
   .finally(async () => {
-    await photon.disconnect()
+    await prisma.disconnect()
   })
