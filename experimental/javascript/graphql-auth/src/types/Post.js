afda38feecc17e33c8ba8a58dd9bdf3e3c61c78b
Daniel Norman <daniel@2color.me>
2020-03-17 11:12:48 +0100

    Remove non-existent fields

--- experimental/javascript/graphql-auth/src/types/Post.js
+++ experimental/javascript/graphql-auth/src/types/Post.js
@@ -4,8 +4,6 @@ const Post = objectType({
   name: 'Post',
   definition(t) {
     t.model.id()
-    t.model.createdAt()
-    t.model.updatedAt()
     t.model.published()
     t.model.title()
     t.model.content()
