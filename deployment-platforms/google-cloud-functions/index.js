7408f5640a82967da8e0efc81123fc6f43f7695e
Nikolas Burk <nikolas.burk@gmail.com>
2020-03-20 08:42:40 +0100

    remove unmaintained deployment examples

--- deployment-platforms/google-cloud-functions/index.js
+++
@@ -1,7 +0,0 @@
-const { Photon } = require('@prisma/photon')
-
-const photon = new Photon()
-exports.photonExample = async function helloWorld(req, res) {
-  const users = await photon.users()
-  res.status(200).send(JSON.stringify(users))
-}
