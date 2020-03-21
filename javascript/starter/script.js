0ec3b023155ef9f3e6e2cf792ff531ab576d109b
Nikolas Burk <nikolas.burk@gmail.com>
2020-03-21 21:35:08 +0100

    add starter for JS quickstart

---
+++ javascript/starter/script.js
@@ -0,0 +1,16 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+// A `main` function so that you can use async/await
+async function main() {
+   // ... you will write your Prisma Client queries here
+}
+
+main()
+  .catch(e => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.disconnect()
+  })
