1089b93577be1263f22fbbfd8be9246f8bd29114
Nikolas <nikolas.burk@gmail.com>
2020-03-30 23:02:05 +0200

    Update script.js

--- javascript/starter/script.js
+++ javascript/starter/script.js
@@ -4,11 +4,7 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  const post = await prisma.post.update({
-    where: { id: 2 },
-    data: { published: true },
-  })
-  console.log(post)
+  // ... you will write your Prisma Client queries here
 }
 
 main()
