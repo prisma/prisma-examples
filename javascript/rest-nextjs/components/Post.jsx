ed9a65f0d62ac307a1a17db6ee2556b7cdd3d357
Shaswat Saxena <shaswatsaxena@gmail.com>
2020-04-03 22:42:05 +0530

    Remove unused dependencies and fixed Post component error (#1702)
    * Fixed Post component nested links error
    
    * Removed unused dependencies: express & tachyons

--- javascript/rest-nextjs/components/Post.jsx
+++ javascript/rest-nextjs/components/Post.jsx
@@ -1,24 +1,20 @@
-import Link from 'next/link'
+import Router from 'next/router'
 import ReactMarkdown from 'react-markdown'
 
 const Post = ({ post }) => {
   const authorName = post.author ? post.author.name : 'Unknown author'
   return (
-    <Link href="/p/[id]" as={`/p/${post.id}`}>
-      <a>
-        <h2>{post.title}</h2>
-        <small>By {authorName}</small>
-        <ReactMarkdown source={post.content} />
-        <style jsx>{`
-          a {
-            text-decoration: none;
-            color: inherit;
-            padding: 2rem;
-            display: block;
-          }
-        `}</style>
-      </a>
-    </Link>
+    <div onClick={() => Router.push('/p/[id]', `/p/${post.id}`)}>
+      <h2>{post.title}</h2>
+      <small>By {authorName}</small>
+      <ReactMarkdown source={post.content} />
+      <style jsx>{`
+        div {
+          color: inherit;
+          padding: 2rem;
+        }
+      `}</style>
+    </div>
   )
 }
 
