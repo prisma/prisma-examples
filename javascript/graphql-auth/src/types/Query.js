5977c6a26756505935b532e7c4b0d44fab8de0af
Jason Kuhrt <jasonkuhrt@me.com>
2020-05-11 14:42:25 -0400

    more renames

--- javascript/graphql-auth/src/types/Query.js
+++ javascript/graphql-auth/src/types/Query.js
@@ -1,4 +1,4 @@
-const { idArg, queryType, stringArg } = require('nexus')
+const { idArg, queryType, stringArg } = require('@nexus/schema')
 const { getUserId } = require('../utils')
 
 const Query = queryType({
