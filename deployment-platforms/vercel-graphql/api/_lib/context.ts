0fc1a0dd69adfffdcd59d709f51973660bbb2900
Daniel Norman <daniel@2color.me>
2020-07-02 12:01:25 +0200

    Log queries

--- deployment-platforms/vercel-graphql/api/_lib/context.ts
+++ deployment-platforms/vercel-graphql/api/_lib/context.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn'],
+})
 
 export interface Context {
   prisma: PrismaClient
