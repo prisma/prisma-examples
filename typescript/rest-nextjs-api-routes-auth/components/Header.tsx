51e554f9bf73f26e2075b342707fb2e874213e48
Nikolas <nikolas.burk@gmail.com>
2020-10-05 12:20:50 +0200

    Next auth (#2057)
    * feat(next-auth): add next-auth example
    
    * feat(next-auth): polish next-auth example
    
    * docs(next-auth): improve readme
    
    * fix(ci): reset @prisma package versions to 2.7.1 where nexus-prisma is used
    
    * chore(next-auth): add tests

---
+++ typescript/rest-nextjs-api-routes-auth/components/Header.tsx
@@ -0,0 +1,211 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { signOut, useSession } from "next-auth/client";
+
+const Header: React.FC = () => {
+  const router = useRouter();
+  const isActive: (pathname: string) => boolean = (pathname) =>
+    router.pathname === pathname;
+
+  const [session, loading] = useSession();
+
+  let left = (
+    <div className="left">
+      <Link href="/">
+        <a className="bold" data-active={isActive("/")}>
+          Feed
+        </a>
+      </Link>
+      <style jsx>{`
+        .bold {
+          font-weight: bold;
+        }
+
+        a {
+          text-decoration: none;
+          color: #000;
+          display: inline-block;
+        }
+
+        .left a[data-active="true"] {
+          color: gray;
+        }
+
+        a + a {
+          margin-left: 1rem;
+        }
+      `}</style>
+    </div>
+  );
+
+  let right = null;
+
+  if (loading) {
+    left = (
+      <div className="left">
+        <Link href="/">
+          <a className="bold" data-active={isActive("/")}>
+            Feed
+          </a>
+        </Link>
+        <style jsx>{`
+          .bold {
+            font-weight: bold;
+          }
+
+          a {
+            text-decoration: none;
+            color: #000;
+            display: inline-block;
+          }
+
+          .left a[data-active="true"] {
+            color: gray;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+        `}</style>
+      </div>
+    );
+    right = (
+      <div className="right">
+        <p>Validating session ...</p>
+        <style jsx>{`
+          .right {
+            margin-left: auto;
+          }
+        `}</style>
+      </div>
+    );
+  }
+
+  if (!session) {
+    right = (
+      <div className="right">
+        <Link href="/api/auth/signin">
+          <a data-active={isActive("/signup")}>Log in</a>
+        </Link>
+        <style jsx>{`
+          a {
+            text-decoration: none;
+            color: #000;
+            display: inline-block;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+
+          .right {
+            margin-left: auto;
+          }
+
+          .right a {
+            border: 1px solid black;
+            padding: 0.5rem 1rem;
+            border-radius: 3px;
+          }
+        `}</style>
+      </div>
+    );
+  }
+
+  if (session) {
+    left = (
+      <div className="left">
+        <Link href="/">
+          <a className="bold" data-active={isActive("/")}>
+            Feed
+          </a>
+        </Link>
+        <Link href="/drafts">
+          <a data-active={isActive("/drafts")}>My drafts</a>
+        </Link>
+        <style jsx>{`
+          .bold {
+            font-weight: bold;
+          }
+
+          a {
+            text-decoration: none;
+            color: #000;
+            display: inline-block;
+          }
+
+          .left a[data-active="true"] {
+            color: gray;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+        `}</style>
+      </div>
+    );
+    right = (
+      <div className="right">
+        <p>
+          {session.user.name} ({session.user.email})
+        </p>
+        <Link href="/create">
+          <button>
+            <a>New post</a>
+          </button>
+        </Link>
+        <button onClick={() => signOut()}>
+          <a>Log out</a>
+        </button>
+        <style jsx>{`
+          a {
+            text-decoration: none;
+            color: #000;
+            display: inline-block;
+          }
+
+          p {
+            display: inline-block;
+            font-size: 13px;
+            padding-right: 1rem;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+
+          .right {
+            margin-left: auto;
+          }
+
+          .right a {
+            border: 1px solid black;
+            padding: 0.5rem 1rem;
+            border-radius: 3px;
+          }
+
+          button {
+            border: none;
+          }
+        `}</style>
+      </div>
+    );
+  }
+
+  return (
+    <nav>
+      {left}
+      {right}
+      <style jsx>{`
+        nav {
+          display: flex;
+          padding: 2rem;
+          align-items: center;
+        }
+      `}</style>
+    </nav>
+  );
+};
+
+export default Header;
