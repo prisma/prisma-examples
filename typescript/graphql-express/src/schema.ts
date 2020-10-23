0299c5a6b842639abca02ea28ac7fb3c9228e5ef
Nikolas Burk <nikolas.burk@gmail.com>
2020-10-23 11:56:27 +0100

    add graphql example with nexus and express-graphql

---
+++ typescript/graphql-express/src/schema.ts
@@ -0,0 +1,123 @@
+import { intArg, makeSchema, objectType, stringArg } from '@nexus/schema'
+import { nexusPrisma } from 'nexus-plugin-prisma'
+
+const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.model.id()
+    t.model.name()
+    t.model.email()
+    t.model.posts({
+      pagination: false,
+    })
+  },
+})
+
+const Post = objectType({
+  name: 'Post',
+  definition(t) {
+    t.model.id()
+    t.model.title()
+    t.model.content()
+    t.model.published()
+    t.model.author()
+  },
+})
+
+const Query = objectType({
+  name: 'Query',
+  definition(t) {
+    t.crud.post()
+
+    t.list.field('feed', {
+      type: 'Post',
+      resolve: (_parent, _args, ctx) => {
+        return ctx.prisma.post.findMany({
+          where: { published: true },
+        })
+      },
+    })
+
+    t.list.field('filterPosts', {
+      type: 'Post',
+      args: {
+        searchString: stringArg({ nullable: true }),
+      },
+      resolve: (_, { searchString }, ctx) => {
+        return ctx.prisma.post.findMany({
+          where: {
+            OR: [
+              { title: { contains: searchString } },
+              { content: { contains: searchString } },
+            ],
+          },
+        })
+      },
+    })
+  },
+})
+
+const Mutation = objectType({
+  name: 'Mutation',
+  definition(t) {
+    t.crud.createOneUser({ alias: 'signupUser' })
+    t.crud.deleteOnePost()
+
+    t.field('createDraft', {
+      type: 'Post',
+      args: {
+        title: stringArg({ nullable: false }),
+        content: stringArg(),
+        authorEmail: stringArg({ nullable: false }),
+      },
+      resolve: (_, { title, content, authorEmail }, ctx) => {
+        return ctx.prisma.post.create({
+          data: {
+            title,
+            content,
+            published: false,
+            author: {
+              connect: { email: authorEmail },
+            },
+          },
+        })
+      },
+    })
+
+    t.field('publish', {
+      type: 'Post',
+      nullable: true,
+      args: {
+        id: intArg(),
+      },
+      resolve: (_, { id }, ctx) => {
+        return ctx.prisma.post.update({
+          where: { id: Number(id) },
+          data: { published: true },
+        })
+      },
+    })
+  },
+})
+
+export const schema = makeSchema({
+  types: [Query, Mutation, Post, User],
+  plugins: [nexusPrisma({ experimentalCRUD: true })],
+  outputs: {
+    schema: __dirname + '/../schema.graphql',
+    typegen: __dirname + '/generated/nexus.ts',
+  },
+  typegenAutoConfig: {
+    contextType: 'Context.Context',
+    sources: [
+      {
+        source: '@prisma/client',
+        alias: 'prisma',
+      },
+      {
+        source: require.resolve('./context'),
+        alias: 'Context',
+      },
+    ],
+  },
+})
