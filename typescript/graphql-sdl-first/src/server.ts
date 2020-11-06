20a4667b625959f9bf59b59bd16b4dd9268ce998
Nikolas <nikolas.burk@gmail.com>
2020-11-06 09:01:04 +0100

    Update server.ts

--- typescript/graphql-sdl-first/src/server.ts
+++ typescript/graphql-sdl-first/src/server.ts
@@ -1,9 +1,11 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { ApolloServer } from 'apollo-server'
 import { schema } from './schema'
 import { createContext } from './context'
 
-new GraphQLServer({ schema, context: createContext }).start(() =>
-  console.log(
-    `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-sdl-first#using-the-graphql-api`,
-  ),
+new ApolloServer({ schema, context: createContext }).listen(
+  { port: 4000 },
+  () =>
+    console.log(
+      `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-apollo-server#using-the-graphql-api`,
+    ),
 )
