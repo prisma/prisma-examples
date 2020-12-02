a6750ca5bed2fb0945199007c9591785036f0e45
Nikolas <nikolas.burk@gmail.com>
2020-12-02 15:29:41 +0100

    Update server.ts

--- typescript/graphql-sdl-first/src/server.ts
+++ typescript/graphql-sdl-first/src/server.ts
@@ -1,8 +1,8 @@
 import { ApolloServer } from 'apollo-server'
-import { schema } from './schema'
+import { typeDefs, resolvers } from './schema'
 import { createContext } from './context'
 
-new ApolloServer({ schema, context: createContext }).listen(
+new ApolloServer({ typeDefs, resolvers, context: createContext }).listen(
   { port: 4000 },
   () =>
     console.log(
