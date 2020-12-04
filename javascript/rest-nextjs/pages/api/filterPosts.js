c820b7f3b9418e9eafe1e374409729766b617ed7
ruheni <aleruheni@outlook.com>
2020-12-04 10:24:29 +0300

    🎉 updated rest-nextjs js example

--- javascript/rest-nextjs/pages/api/filterPosts.js
+++ javascript/rest-nextjs/pages/api/filterPosts.js
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from '../../lib/prisma'
 
 // GET /api/filterPosts?searchString=:searchString
 export default async function handle(req, res) {
