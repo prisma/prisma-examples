4edba47133342c14c455faca3a99be9b3dc10605
Divyendu Singh <singh@prisma.io>
2020-12-11 14:23:40 +0100

    chore: fix linked action link

--- .github/scripts/check-for-update.sh
+++ .github/scripts/check-for-update.sh
@@ -116,7 +116,7 @@ while [ $i -le $count ]; do
 
     export webhook="$SLACK_WEBHOOK_URL_FAILING"
     (cd .github/slack/ && yarn install)
-    node .github/slack/notify.js "Prisma version $v released via the action https://github.com/prisma/prisma-examples/runs/$GITHUB_RUN_ID?check_suite_focus=true"
+    node .github/slack/notify.js "Prisma version $v released via the action https://github.com/prisma/prisma-examples/actions/runs/$GITHUB_RUN_ID?check_suite_focus=true"
   fi
 
   echo "pushed commit"
