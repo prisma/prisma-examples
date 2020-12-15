d753713d1895b459e89e0c5dee7c040696bc3a87
Jason Kuhrt <jasonkuhrt@me.com>
2020-12-15 10:03:48 -0500

    migrate rest

--- typescript/graphql-hapi/src/schema.ts
+++ typescript/graphql-hapi/src/schema.ts
@@ -1,123 +1,129 @@
-import { intArg, makeSchema, objectType, stringArg } from '@nexus/schema'
+import {
+  intArg,
+  makeSchema,
+  nonNull,
+  nullable,
+  objectType,
+  stringArg,
+} from 'nexus'
 import { nexusPrisma } from 'nexus-plugin-prisma'
 
 const User = objectType({
-    name: 'User',
-    definition(t) {
-        t.model.id()
-        t.model.name()
-        t.model.email()
-        t.model.posts({
-            pagination: false,
-        })
-    },
+  name: 'User',
+  definition(t) {
+    t.model.id()
+    t.model.name()
+    t.model.email()
+    t.model.posts({
+      pagination: false,
+    })
+  },
 })
 
 const Post = objectType({
-    name: 'Post',
-    definition(t) {
-        t.model.id()
-        t.model.title()
-        t.model.content()
-        t.model.published()
-        t.model.author()
-    },
+  name: 'Post',
+  definition(t) {
+    t.model.id()
+    t.model.title()
+    t.model.content()
+    t.model.published()
+    t.model.author()
+  },
 })
 
 const Query = objectType({
-    name: 'Query',
-    definition(t) {
-        t.crud.post()
+  name: 'Query',
+  definition(t) {
+    t.crud.post()
 
-        t.list.field('feed', {
-            type: 'Post',
-            resolve: (_parent, _args, ctx) => {
-                return ctx.prisma.post.findMany({
-                    where: { published: true },
-                })
-            },
+    t.list.field('feed', {
+      type: 'Post',
+      resolve: (_parent, _args, ctx) => {
+        return ctx.prisma.post.findMany({
+          where: { published: true },
         })
+      },
+    })
 
-        t.list.field('filterPosts', {
-            type: 'Post',
-            args: {
-                searchString: stringArg({ nullable: true }),
-            },
-            resolve: (_, { searchString }, ctx) => {
-                return ctx.prisma.post.findMany({
-                    where: {
-                        OR: [
-                            { title: { contains: searchString } },
-                            { content: { contains: searchString } },
-                        ],
-                    },
-                })
-            },
+    t.list.field('filterPosts', {
+      type: 'Post',
+      args: {
+        searchString: nullable(stringArg()),
+      },
+      resolve: (_, { searchString }, ctx) => {
+        return ctx.prisma.post.findMany({
+          where: {
+            OR: [
+              { title: { contains: searchString } },
+              { content: { contains: searchString } },
+            ],
+          },
         })
-    },
+      },
+    })
+  },
 })
 
 const Mutation = objectType({
-    name: 'Mutation',
-    definition(t) {
-        t.crud.createOneUser({ alias: 'signupUser' })
-        t.crud.deleteOnePost()
+  name: 'Mutation',
+  definition(t) {
+    t.crud.createOneUser({ alias: 'signupUser' })
+    t.crud.deleteOnePost()
 
-        t.field('createDraft', {
-            type: 'Post',
-            args: {
-                title: stringArg({ nullable: false }),
-                content: stringArg(),
-                authorEmail: stringArg({ nullable: false }),
+    t.field('createDraft', {
+      type: 'Post',
+      args: {
+        title: nonNull(stringArg()),
+        content: stringArg(),
+        authorEmail: nonNull(stringArg()),
+      },
+      resolve: (_, { title, content, authorEmail }, ctx) => {
+        return ctx.prisma.post.create({
+          data: {
+            title,
+            content,
+            published: false,
+            author: {
+              connect: { email: authorEmail },
             },
-            resolve: (_, { title, content, authorEmail }, ctx) => {
-                return ctx.prisma.post.create({
-                    data: {
-                        title,
-                        content,
-                        published: false,
-                        author: {
-                            connect: { email: authorEmail },
-                        },
-                    },
-                })
-            },
+          },
         })
+      },
+    })
 
-        t.field('publish', {
-            type: 'Post',
-            nullable: true,
-            args: {
-                id: intArg(),
-            },
-            resolve: (_, { id }, ctx) => {
-                return ctx.prisma.post.update({
-                    where: { id: Number(id) },
-                    data: { published: true },
-                })
-            },
+    t.nullable.field('publish', {
+      type: 'Post',
+      args: {
+        id: intArg(),
+      },
+      resolve: (_, { id }, ctx) => {
+        return ctx.prisma.post.update({
+          where: { id: Number(id) },
+          data: { published: true },
         })
-    },
+      },
+    })
+  },
 })
 
 export const schema = makeSchema({
-    types: [Query, Mutation, Post, User],
-    plugins: [nexusPrisma({ experimentalCRUD: true })],
-    outputs: {
-        schema: __dirname + '/../schema.graphql',
-        typegen: __dirname + '/generated/nexus.ts',
-    },
-    typegenAutoConfig: {
-        contextType: 'Context.Context',
-        sources: [
-            {
-                source: '@prisma/client',
-                alias: 'prisma',
-            },
-            {
-                source: require.resolve('./context'),
-                alias: 'Context',
-            },
-        ],
-    },
+  types: [Query, Mutation, Post, User],
+  plugins: [nexusPrisma({ experimentalCRUD: true })],
+  outputs: {
+    schema: __dirname + '/../schema.graphql',
+    typegen: __dirname + '/generated/nexus.ts',
+  },
+  contextType: {
+    module: require.resolve('./context'),
+    export: 'Context',
+  },
+  sourceTypes: {
+    modules: [
+      {
+        module: '@prisma/client',
+        alias: 'prisma',
+      },
+      ,
+    ],
+  },
 })
