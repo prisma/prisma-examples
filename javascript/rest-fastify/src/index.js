d2d9161939f9154a0a851ee76ced3623d2035244
aperrix <aperrix@gmail.com>
2020-12-18 12:37:55 +0100

    improvements

---
+++ javascript/rest-fastify/src/index.js
@@ -0,0 +1,104 @@
+const { PrismaClient } = require('@prisma/client')
+const app = require('fastify')({ logger: true })
+
+const prisma = new PrismaClient()
+
+app.register(require('fastify-formbody'))
+app.register(require("fastify-blipp"))
+
+app.post(`/user`, async (req, res) => {
+  const result = await prisma.user.create({
+    data: {
+      ...req.body,
+    },
+  })
+  return result
+})
+
+app.post(`/post`, async (req, res) => {
+  const { title, content, authorEmail } = req.body
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      published: false,
+      author: { connect: { email: authorEmail } },
+    },
+  })
+  return result
+})
+
+app.put('/publish/:id', async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: { published: true },
+  })
+  return post
+})
+
+app.delete(`/post/:id`, async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.delete({
+    where: {
+      id: parseInt(id),
+    },
+  })
+  return post
+})
+
+app.get(`/post/:id`, async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.findOne({
+    where: {
+      id: parseInt(id),
+    },
+  })
+  return post
+})
+
+app.get('/feed', async (req, res) => {
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: { author: true },
+  })
+  return posts
+})
+
+app.get('/filterPosts', async (req, res) => {
+  const { searchString } = req.query
+  const draftPosts = await prisma.post.findMany({
+    where: {
+      OR: [
+        {
+          title: {
+            contains: searchString,
+          },
+        },
+        {
+          content: {
+            contains: searchString,
+          },
+        },
+      ],
+    },
+  })
+  return draftPosts
+})
+
+const start = async () => {
+  try {
+    await app.listen(3000);
+ 
+    app.blipp();
+ 
+    console.log(`server listening on ${app.server.address().port}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+ 
+start();
