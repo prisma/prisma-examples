73be2e69b989fb922ed36b3d120165acc1093fab
Nikolas Burk <nikolas.burk@gmail.com>
2021-02-01 15:58:36 +0100

    update standard graphql example

--- typescript/graphql/src/context.ts
+++ typescript/graphql/src/context.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
-
 export interface Context {
   prisma: PrismaClient
 }
 
-export function createContext(): Context {
-  return { prisma }
+const prisma = new PrismaClient()
+
+export const context: Context = {
+  prisma: prisma,
 }
--- typescript/graphql/src/schema.ts
+++ typescript/graphql/src/schema.ts
@@ -2,62 +2,84 @@ import {
   intArg,
   makeSchema,
   nonNull,
-  nullable,
   objectType,
   stringArg,
+  inputObjectType,
+  arg,
+  asNexusMethod,
+  enumType,
 } from 'nexus'
-import { nexusPrisma } from 'nexus-plugin-prisma'
+import { GraphQLDateTime } from 'graphql-iso-date'
+import { Prisma } from '@prisma/client'
 
-const User = objectType({
-  name: 'User',
-  definition(t) {
-    t.model.id()
-    t.model.name()
-    t.model.email()
-    t.model.posts({
-      pagination: false,
-    })
-  },
-})
-
-const Post = objectType({
-  name: 'Post',
-  definition(t) {
-    t.model.id()
-    t.model.title()
-    t.model.content()
-    t.model.published()
-    t.model.author()
-  },
-})
+export const DateTime = asNexusMethod(GraphQLDateTime, 'date')
 
 const Query = objectType({
   name: 'Query',
   definition(t) {
-    t.crud.post()
+    t.string('hello', {
+      resolve: () => 'Hello World',
+    })
 
-    t.list.field('feed', {
+    t.nonNull.list.nonNull.field('feed', {
       type: 'Post',
-      resolve: (_parent, _args, ctx) => {
-        return ctx.prisma.post.findMany({
-          where: { published: true },
+      args: {
+        searchString: stringArg(),
+        skip: intArg(),
+        take: intArg(),
+        orderBy: arg({
+          type: 'PostOrderBy',
+        }),
+      },
+      resolve: (_parent, args, context) => {
+        return context.prisma.post.findMany({
+          where: {
+            published: true,
+            OR: [
+              { title: { contains: args.searchString || undefined } },
+              { content: { contains: args.searchString || undefined } },
+            ],
+          },
+          take: args.take || undefined,
+          skip: args.skip || undefined,
+          orderBy: transformOrderBy(args.orderBy),
         })
       },
     })
 
-    t.list.field('filterPosts', {
+    t.list.field('draftsByUser', {
       type: 'Post',
       args: {
-        searchString: nullable(stringArg()),
+        userUniqueInput: nonNull(
+          arg({
+            type: 'UserUniqueInput',
+          }),
+        ),
       },
-      resolve: (_, { searchString }, ctx) => {
-        return ctx.prisma.post.findMany({
-          where: {
-            OR: [
-              { title: { contains: searchString } },
-              { content: { contains: searchString } },
-            ],
-          },
+      resolve: (_parent, args, context) => {
+        return context.prisma.user
+          .findUnique({
+            where: {
+              id: args.userUniqueInput.id || undefined,
+              email: args.userUniqueInput.email || undefined,
+            },
+          })
+          .posts({
+            where: {
+              published: true,
+            },
+          })
+      },
+    })
+
+    t.nullable.field('postById', {
+      type: 'Post',
+      args: {
+        id: intArg(),
+      },
+      resolve: (_parent, args, context) => {
+        return context.prisma.post.findUnique({
+          where: { id: args.id || undefined },
         })
       },
     })
@@ -67,48 +89,205 @@ const Query = objectType({
 const Mutation = objectType({
   name: 'Mutation',
   definition(t) {
-    t.crud.createOneUser({ alias: 'signupUser' })
-    t.crud.deleteOnePost()
-
-    t.field('createDraft', {
-      type: 'Post',
+    t.nonNull.field('signupUser', {
+      type: 'User',
       args: {
-        title: nonNull(stringArg()),
-        content: stringArg(),
-        authorEmail: nonNull(stringArg()),
+        data: nonNull(
+          arg({
+            type: 'UserCreateInput',
+          }),
+        ),
       },
-      resolve: (_, { title, content, authorEmail }, ctx) => {
-        return ctx.prisma.post.create({
+      resolve: (_, args, context) => {
+        const postData = args.data.posts.map((post) => {
+          return { title: post.title, content: post.content || undefined }
+        })
+        return context.prisma.user.create({
           data: {
-            title,
-            content,
-            published: false,
-            author: {
-              connect: { email: authorEmail },
+            name: args.data.name,
+            email: args.data.email,
+            posts: {
+              create: postData,
             },
           },
         })
       },
     })
 
-    t.nullable.field('publish', {
+    t.field('createDraft', {
       type: 'Post',
       args: {
-        id: intArg(),
+        data: nonNull(
+          arg({
+            type: 'PostCreateInput',
+          }),
+        ),
+        authorEmail: nonNull(stringArg()),
       },
-      resolve: (_, { id }, ctx) => {
-        return ctx.prisma.post.update({
-          where: { id: Number(id) },
-          data: { published: true },
+      resolve: (_, args, context) => {
+        return context.prisma.post.create({
+          data: {
+            title: args.data.title,
+            content: args.data.content,
+            author: {
+              connect: { email: args.authorEmail },
+            },
+          },
+        })
+      },
+    })
+
+    t.field('togglePublishPost', {
+      type: 'Post',
+      args: {
+        id: nonNull(intArg()),
+      },
+      resolve: async (_, args, context) => {
+        try {
+          const post = await context.prisma.post.findUnique({
+            where: { id: args.id || undefined },
+            select: {
+              published: true,
+            },
+          })
+          return context.prisma.post.update({
+            where: { id: args.id || undefined },
+            data: { published: !post?.published },
+          })
+        } catch (e) {
+          throw new Error(
+            `Post with ID ${args.id} does not exist in the database.`,
+          )
+        }
+      },
+    })
+
+    t.field('incrementPostViewCount', {
+      type: 'Post',
+      args: {
+        id: nonNull(intArg()),
+      },
+      resolve: (_, args, context) => {
+        return context.prisma.post.update({
+          where: { id: args.id || undefined },
+          data: {
+            viewCount: {
+              increment: 1,
+            },
+          },
+        })
+      },
+    })
+
+    t.field('deletePost', {
+      type: 'Post',
+      args: {
+        id: nonNull(intArg()),
+      },
+      resolve: (_, args, context) => {
+        return context.prisma.post.delete({
+          where: { id: args.id },
         })
       },
     })
   },
 })
 
+const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.nonNull.int('id')
+    t.string('name')
+    t.nonNull.string('email')
+    t.nonNull.list.nonNull.field('posts', {
+      type: 'Post',
+      resolve: (parent, _, context) => {
+        return context.prisma.user
+          .findUnique({
+            where: { id: parent.id || undefined },
+          })
+          .posts()
+      },
+    })
+  },
+})
+
+const Post = objectType({
+  name: 'Post',
+  definition(t) {
+    t.nonNull.int('id')
+    t.nonNull.field('createdAt', {
+      type: 'DateTime',
+    })
+    t.nonNull.field('updatedAt', {
+      type: 'DateTime',
+    })
+    t.nonNull.string('title')
+    t.string('content')
+    t.nonNull.boolean('published')
+    t.nonNull.int('viewCount')
+    t.field('author', {
+      type: 'User',
+      resolve: (parent, _, context) => {
+        return context.prisma.post
+          .findUnique({
+            where: { id: parent.id || undefined },
+          })
+          .author()
+      },
+    })
+  },
+})
+
+const PostOrderBy = enumType({
+  name: 'PostOrderBy',
+  members: [
+    'createdAt_asc',
+    'createdAt_desc',
+    'updatedAt_asc',
+    'updatedAt_desc',
+  ],
+})
+
+const UserUniqueInput = inputObjectType({
+  name: 'UserUniqueInput',
+  definition(t) {
+    t.int('id')
+    t.string('email')
+  },
+})
+
+const PostCreateInput = inputObjectType({
+  name: 'PostCreateInput',
+  definition(t) {
+    t.nonNull.string('title')
+    t.string('content')
+  },
+})
+
+const UserCreateInput = inputObjectType({
+  name: 'UserCreateInput',
+  definition(t) {
+    t.nonNull.string('email')
+    t.string('name')
+    t.nonNull.list.nonNull.field('posts', {
+      type: 'PostCreateInput',
+    })
+  },
+})
+
 export const schema = makeSchema({
-  types: [Query, Mutation, Post, User],
-  plugins: [nexusPrisma({ experimentalCRUD: true })],
+  types: [
+    Query,
+    Mutation,
+    Post,
+    User,
+    UserUniqueInput,
+    UserCreateInput,
+    PostCreateInput,
+    PostOrderBy,
+    DateTime,
+  ],
   outputs: {
     schema: __dirname + '/../schema.graphql',
     typegen: __dirname + '/generated/nexus.ts',
@@ -126,3 +305,14 @@ export const schema = makeSchema({
     ],
   },
 })
+
+function transformOrderBy(
+  orderBy: string | null | undefined,
+): Prisma.PostOrderByInput | undefined {
+  if (!orderBy) {
+    return undefined
+  }
+  const raw = orderBy.split('_') as [string, string]
+  const map = new Map([raw])
+  return Object.fromEntries(map)
+}
