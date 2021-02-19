c75857284153c46489369dc8b2f02abffa8bf127
Nikolas <nikolas.burk@gmail.com>
2021-02-19 10:18:28 +0100

    refactor: ts graphql-auth example (#2559)
    * refactor: ts graphql-auth example
    
    * refactor: ts graphql-auth example
    
    * refactor: remove dev.db
    
    * refactor: update README
    
    * refactor: update README
    
    * refactor: update README
    
    * refactor: update README
    
    * refactor: update README
    
    * refactor: update README
    
    * refactor: update README
    
    * refactor: update README

--- typescript/graphql-auth/src/schema.ts
+++ typescript/graphql-auth/src/schema.ts
@@ -1,30 +1,390 @@
-import { applyMiddleware } from 'graphql-middleware'
-import { makeSchema } from 'nexus'
-import { nexusPrisma } from 'nexus-plugin-prisma'
 import { permissions } from './permissions'
-import * as types from './types'
+import { APP_SECRET, getUserId } from './utils'
+import { compare, hash } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+import { applyMiddleware } from 'graphql-middleware'
+// import * as types from './types'
+import {
+  intArg,
+  makeSchema,
+  nonNull,
+  objectType,
+  stringArg,
+  inputObjectType,
+  arg,
+  asNexusMethod,
+  enumType,
+} from 'nexus'
+import { GraphQLDateTime } from 'graphql-iso-date'
 
-export const schema = applyMiddleware(
-  makeSchema({
-    types,
-    plugins: [nexusPrisma()],
-    outputs: {
-      schema: __dirname + '/../schema.graphql',
-      typegen: __dirname + '/generated/nexus.ts',
-    },
-    contextType: {
-      module: require.resolve('./context'),
-      alias: 'Context',
-      export: 'Context',
-    },
-    sourceTypes: {
-      modules: [
-        {
-          module: '@prisma/client',
-          alias: 'client',
-        },
-      ],
-    },
-  }),
-  permissions,
-)
+export const DateTime = asNexusMethod(GraphQLDateTime, 'date')
+
+const Query = objectType({
+  name: 'Query',
+  definition(t) {
+    t.nonNull.list.nonNull.field('allUsers', {
+      type: 'User',
+      resolve: (_parent, _args, context) => {
+        console.log(`allUsers`)
+        return context.prisma.user.findMany()
+      },
+    })
+
+    t.nullable.field('me', {
+      type: 'User',
+      resolve: (parent, args, context) => {
+        const userId = getUserId(context)
+        return context.prisma.user.findUnique({
+          where: {
+            id: Number(userId),
+          },
+        })
+      },
+    })
+
+    t.nullable.field('postById', {
+      type: 'Post',
+      args: {
+        id: intArg(),
+      },
+      resolve: (_parent, args, context) => {
+        return context.prisma.post.findUnique({
+          where: { id: args.id || undefined },
+        })
+      },
+    })
+
+    t.nonNull.list.nonNull.field('feed', {
+      type: 'Post',
+      args: {
+        searchString: stringArg(),
+        skip: intArg(),
+        take: intArg(),
+        orderBy: arg({
+          type: 'PostOrderByUpdatedAtInput',
+        }),
+      },
+      resolve: (_parent, args, context) => {
+        const or = args.searchString
+          ? {
+              OR: [
+                { title: { contains: args.searchString } },
+                { content: { contains: args.searchString } },
+              ],
+            }
+          : {}
+
+        return context.prisma.post.findMany({
+          where: {
+            published: true,
+            ...or,
+          },
+          take: args.take || undefined,
+          skip: args.skip || undefined,
+          orderBy: args.orderBy || undefined,
+        })
+      },
+    })
+
+    t.list.field('draftsByUser', {
+      type: 'Post',
+      args: {
+        userUniqueInput: nonNull(
+          arg({
+            type: 'UserUniqueInput',
+          }),
+        ),
+      },
+      resolve: (_parent, args, context) => {
+        return context.prisma.user
+          .findUnique({
+            where: {
+              id: args.userUniqueInput.id || undefined,
+              email: args.userUniqueInput.email || undefined,
+            },
+          })
+          .posts({
+            where: {
+              published: false,
+            },
+          })
+      },
+    })
+  },
+})
+
+const Mutation = objectType({
+  name: 'Mutation',
+  definition(t) {
+    t.field('signup', {
+      type: 'AuthPayload',
+      args: {
+        name: stringArg(),
+        email: nonNull(stringArg()),
+        password: nonNull(stringArg()),
+      },
+      resolve: async (_parent, args, context) => {
+        const hashedPassword = await hash(args.password, 10)
+        const user = await context.prisma.user.create({
+          data: {
+            name: args.name,
+            email: args.email,
+            password: hashedPassword,
+          },
+        })
+        return {
+          token: sign({ userId: user.id }, APP_SECRET),
+          user,
+        }
+      },
+    })
+
+    t.field('login', {
+      type: 'AuthPayload',
+      args: {
+        email: nonNull(stringArg()),
+        password: nonNull(stringArg()),
+      },
+      resolve: async (_parent, { email, password }, context) => {
+        const user = await context.prisma.user.findUnique({
+          where: {
+            email,
+          },
+        })
+        if (!user) {
+          throw new Error(`No user found for email: ${email}`)
+        }
+        const passwordValid = await compare(password, user.password)
+        if (!passwordValid) {
+          throw new Error('Invalid password')
+        }
+        return {
+          token: sign({ userId: user.id }, APP_SECRET),
+          user,
+        }
+      },
+    })
+
+    t.field('createDraft', {
+      type: 'Post',
+      args: {
+        data: nonNull(
+          arg({
+            type: 'PostCreateInput',
+          }),
+        ),
+      },
+      resolve: (_, args, context) => {
+        const userId = getUserId(context)
+        return context.prisma.post.create({
+          data: {
+            title: args.data.title,
+            content: args.data.content,
+            authorId: userId,
+          },
+        })
+      },
+    })
+
+    t.field('togglePublishPost', {
+      type: 'Post',
+      args: {
+        id: nonNull(intArg()),
+      },
+      resolve: async (_, args, context) => {
+        try {
+          const post = await context.prisma.post.findUnique({
+            where: { id: args.id || undefined },
+            select: {
+              published: true,
+            },
+          })
+          return context.prisma.post.update({
+            where: { id: args.id || undefined },
+            data: { published: !post?.published },
+          })
+        } catch (e) {
+          throw new Error(
+            `Post with ID ${args.id} does not exist in the database.`,
+          )
+        }
+      },
+    })
+
+    t.field('incrementPostViewCount', {
+      type: 'Post',
+      args: {
+        id: nonNull(intArg()),
+      },
+      resolve: (_, args, context) => {
+        return context.prisma.post.update({
+          where: { id: args.id || undefined },
+          data: {
+            viewCount: {
+              increment: 1,
+            },
+          },
+        })
+      },
+    })
+
+    t.field('deletePost', {
+      type: 'Post',
+      args: {
+        id: nonNull(intArg()),
+      },
+      resolve: (_, args, context) => {
+        return context.prisma.post.delete({
+          where: { id: args.id },
+        })
+      },
+    })
+  },
+})
+
+const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.nonNull.int('id')
+    t.string('name')
+    t.nonNull.string('email')
+    t.nonNull.list.nonNull.field('posts', {
+      type: 'Post',
+      resolve: (parent, _, context) => {
+        return context.prisma.user
+          .findUnique({
+            where: { id: parent.id || undefined },
+          })
+          .posts()
+      },
+    })
+  },
+})
+
+const Post = objectType({
+  name: 'Post',
+  definition(t) {
+    t.nonNull.int('id')
+    t.nonNull.field('createdAt', { type: 'DateTime' })
+    t.nonNull.field('updatedAt', { type: 'DateTime' })
+    t.nonNull.string('title')
+    t.string('content')
+    t.nonNull.boolean('published')
+    t.nonNull.int('viewCount')
+    t.field('author', {
+      type: 'User',
+      resolve: (parent, _, context) => {
+        return context.prisma.post
+          .findUnique({
+            where: { id: parent.id || undefined },
+          })
+          .author()
+      },
+    })
+  },
+})
+
+const SortOrder = enumType({
+  name: 'SortOrder',
+  members: ['asc', 'desc'],
+})
+
+const PostOrderByUpdatedAtInput = inputObjectType({
+  name: 'PostOrderByUpdatedAtInput',
+  definition(t) {
+    t.nonNull.field('updatedAt', { type: 'SortOrder' })
+  },
+})
+
+const UserUniqueInput = inputObjectType({
+  name: 'UserUniqueInput',
+  definition(t) {
+    t.int('id')
+    t.string('email')
+  },
+})
+
+const PostCreateInput = inputObjectType({
+  name: 'PostCreateInput',
+  definition(t) {
+    t.nonNull.string('title')
+    t.string('content')
+  },
+})
+
+const UserCreateInput = inputObjectType({
+  name: 'UserCreateInput',
+  definition(t) {
+    t.nonNull.string('email')
+    t.string('name')
+    t.list.nonNull.field('posts', { type: 'PostCreateInput' })
+  },
+})
+
+const AuthPayload = objectType({
+  name: 'AuthPayload',
+  definition(t) {
+    t.string('token')
+    t.field('user', { type: 'User' })
+  },
+})
+
+const schemaWithoutPermissions = makeSchema({
+  types: [
+    Query,
+    Mutation,
+    Post,
+    User,
+    AuthPayload,
+    UserUniqueInput,
+    UserCreateInput,
+    PostCreateInput,
+    SortOrder,
+    PostOrderByUpdatedAtInput,
+    DateTime,
+  ],
+  outputs: {
+    schema: __dirname + '/../schema.graphql',
+    typegen: __dirname + '/generated/nexus.ts',
+  },
+  contextType: {
+    module: require.resolve('./context'),
+    export: 'Context',
+  },
+  sourceTypes: {
+    modules: [
+      {
+        module: '@prisma/client',
+        alias: 'prisma',
+      },
+    ],
+  },
+})
+
+export const schema = schemaWithoutPermissions
+// export const schema = applyMiddleware(schemaWithoutPermissions, permissions)
+
+// export const schema = applyMiddleware(
+//   makeSchema({
+//     types,
+//     plugins: [nexusPrisma()],
+//     outputs: {
+//       schema: __dirname + '/../schema.graphql',
+//       typegen: __dirname + '/generated/nexus.ts',
+//     },
+//     contextType: {
+//       module: require.resolve('./context'),
+//       alias: 'Context',
+//       export: 'Context',
+//     },
+//     sourceTypes: {
+//       modules: [
+//         {
+//           module: '@prisma/client',
+//           alias: 'client',
+//         },
+//       ],
+//     },
+//   }),
+//   permissions,
+// )
--- typescript/graphql-hapi/README.md
+++ typescript/graphql-hapi/README.md
@@ -155,7 +155,7 @@ mutation {
 }
 ```
 
-### Publish/unpublish an existing draft
+### Publish/unpublish an existing post
 
 ```graphql
 mutation {
