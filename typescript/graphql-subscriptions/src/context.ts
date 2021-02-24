79f8fff3a989fa55d30b550d8ccd55d55f76f996
Nikolas Burk <nikolas.burk@gmail.com>
2021-02-24 12:58:48 +0100

    cleanup graphql subscriptions example

--- typescript/graphql-subscriptions/src/context.ts
+++ typescript/graphql-subscriptions/src/context.ts
@@ -1,21 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 import { PubSub } from 'apollo-server'
-import { Request, Response } from 'express'
 
 export interface Context {
   prisma: PrismaClient
-  req: Request
-  res: Response
   pubsub: PubSub
 }
 
 const prisma = new PrismaClient()
 const pubsub = new PubSub()
 
-export const createContext = (ctx: any): Context => {
-  return {
-    ...ctx,
-    prisma,
-    pubsub,
-  }
+export const context: Context = {
+  prisma: prisma,
+  pubsub: pubsub,
 }
