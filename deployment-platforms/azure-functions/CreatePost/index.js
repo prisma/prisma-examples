bf60edef9d9fd4ed66baf7925c75082373f4fc8d
Daniel Norman <daniel@2color.me>
2021-03-02 14:45:36 +0100

    wrap with try and catch and log error

--- deployment-platforms/azure-functions/CreatePost/index.js
+++ deployment-platforms/azure-functions/CreatePost/index.js
@@ -2,21 +2,27 @@ const prisma = require('../lib/prisma')
 
 module.exports = async function (context, req) {
   const { title, content, authorEmail } = req.body
-
-  const post = await prisma.post.create({
-    data: {
-      title,
-      content,
-      published: false,
-      author: {
-        connect: {
-          email: authorEmail,
+  try {
+    const post = await prisma.post.create({
+      data: {
+        title,
+        content,
+        published: false,
+        author: {
+          connect: {
+            email: authorEmail,
+          },
         },
       },
-    },
-  })
+    })
 
-  return  {
-    body: post,
+    return {
+      body: post,
+    }
+  } catch (e) {
+    context.log(e)
+    return {
+      status: 500
+    }
   }
 }
--- deployment-platforms/azure-functions/GetPost/index.js
+++ deployment-platforms/azure-functions/GetPost/index.js
@@ -2,15 +2,21 @@ const prisma = require('../lib/prisma')
 
 module.exports = async function (context, req) {
   const { id } = req.query
+  try {
+    const post = await prisma.post.findUnique({
+      where: {
+        id: parseInt(id, 10),
+      },
+    })
 
-  const post = await prisma.post.findUnique({
-    where: {
-      id: parseInt(id, 10),
-    },
-  })
-
-  return  {
-    status: 200,
-    body: post,
+    return {
+      status: 200,
+      body: post,
+    }
+  } catch (e) {
+    context.log(e)
+    return {
+      status: 500,
+    }
   }
 }
