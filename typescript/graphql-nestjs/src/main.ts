e68a7ed124718af530a2c26b40bc8a78f0c90162
ruheni <aleruheni@outlook.com>
2021-03-11 16:26:27 +0300

    added console.log to nestjs examples

--- typescript/graphql-nestjs/src/main.ts
+++ typescript/graphql-nestjs/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(3000, () => {
+    console.log(`
+🚀 Server ready at: http://localhost:3000/graphql
+⭐️ See sample queries: http://pris.ly/e/ts/graphql-nestjs#using-the-graphql-api
+`)
+  })
 }
 bootstrap()
