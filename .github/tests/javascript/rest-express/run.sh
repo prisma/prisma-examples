df12baf0c6f977430fe63361b91ae62fba8f2521
ruheni <aleruheni@outlook.com>
2021-04-02 23:06:58 +0300

    reset rest-express test

--- .github/tests/javascript/rest-express/run.sh
+++ .github/tests/javascript/rest-express/run.sh
@@ -2,14 +2,13 @@
 
 set -eu
 
-npm install
-npx prisma migrate dev --name init
-npx prisma db seed --preview-feature
-npm run dev &
+yarn 
+yarn prisma db push --preview-feature
+yarn dev &
 pid=$!
 
 sleep 15
 
-npx newman run https://gist.githubusercontent.com/ruheni/3467b4dc135e537d422e5f0f0f80e602/raw/7b586cccb18d6577eb4ccd12d1d13c09c267db50/prisma-examples-rest.postman_collection.json --bail
+curl --fail 'http://localhost:3000/feed?searchString=Prisma'
 
 kill "$pid"
