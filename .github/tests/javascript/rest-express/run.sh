9e56b3990527c7b9a3ff564bd0117cfb6130b695
ruheni <aleruheni@outlook.com>
2021-04-02 22:43:05 +0300

    minor mod to test

--- .github/tests/javascript/rest-express/run.sh
+++ .github/tests/javascript/rest-express/run.sh
@@ -10,6 +10,6 @@ pid=$!
 
 sleep 15
 
-npx newman run ../../postman_collections/rest.json --bail
+npx newman run https://gist.githubusercontent.com/ruheni/3467b4dc135e537d422e5f0f0f80e602/raw/7b586cccb18d6577eb4ccd12d1d13c09c267db50/prisma-examples-rest.postman_collection.json --bail
 
 kill "$pid"
