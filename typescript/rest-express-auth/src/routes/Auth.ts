f14f40cf3ff2722adb1504acfb79680cfb9d475b
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-05-18 13:03:13 -0700

    refactor: start some work on signup

--- typescript/rest-express-auth/src/routes/Auth.ts
+++ typescript/rest-express-auth/src/routes/Auth.ts
@@ -1,9 +1,36 @@
 import { prisma } from '../lib/prisma'
 import { Router } from 'express'
+import argon2 from 'argon2'
 
 export const auth = Router()
 
-auth.post('/signup', async (req, res) => {})
+auth.post('/signup', async (req, res) => {
+  const { email, password, name } = req.body
+
+  const result = await prisma.user.findUnique({
+    where: {
+      name,
+    },
+  })
+
+  if (result) return res.json({ success: false, error: 'User already exists' })
+
+  if (result)
+    return res
+      .status(400)
+      .json({ success: false, error: 'User already exists' })
+
+  const hashedPassword = await argon2.hash(password)
+
+  const user = await prisma.user.create({
+    data: {
+      email: email,
+      password: hashedPassword,
+      name: name,
+      sessionKey: '',
+    },
+  })
+})
 
 auth.post('/login', async (req, res) => {})
 
