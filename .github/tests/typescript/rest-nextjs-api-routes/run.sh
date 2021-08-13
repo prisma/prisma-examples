3208fc17e0bb26060e2cd1b822e5118bd7bb5a16
Poorshad <p.shaddel@gmail.com>
2021-08-13 23:23:25 +0430

    test: Add e2e tests to js and ts bash scripts

--- .github/tests/typescript/rest-nextjs-api-routes/run.sh
+++ .github/tests/typescript/rest-nextjs-api-routes/run.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-yarn 
+yarn
 yarn prisma db push --preview-feature
 yarn dev &
 pid=$!
@@ -13,5 +13,9 @@ curl --fail 'http://localhost:3000/api/filterPosts?searchString=Prisma'
 
 # check frontend
 curl --fail 'http://localhost:3000/'
+
+# e2e tests
+npx prisma db seed --preview-feature
+npx playwright test ../../../tests/e2e/rest-nextjs/test.spec.ts
 
 kill "$pid"
