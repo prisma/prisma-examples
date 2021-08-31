96d59b6d1f0a143f4242767b5ed69d99597ce8ec
m-abdelwahab <hey@mahmoud.codes>
2021-09-01 01:50:13 +0200

    chore: update sdl-first graphql example

--- javascript/graphql-sdl-first/src/server.js
+++ javascript/graphql-sdl-first/src/server.js
@@ -1,14 +1,11 @@
 const { ApolloServer } = require('apollo-server')
-const { schema } = require('./schema')
+const { typeDefs, resolvers } = require('./schema')
 const { context } = require('./context')
 
-const server = new ApolloServer({ schema, context: context })
+const server = new ApolloServer({ typeDefs, resolvers, context: context })
 
-server
-  .listen()
-  .then(({ url }) =>
-    console.log(`
+server.listen().then(({ url }) =>
+  console.log(`
 🚀 Server ready at: ${url}
-⭐️ See sample queries: http://pris.ly/e/js/graphql-sdl-first#using-the-graphql-api`,
-    ),
-  )
+⭐️ See sample queries: http://pris.ly/e/js/graphql-sdl-first#using-the-graphql-api`),
+)
