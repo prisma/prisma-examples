69562d06364f87b63a52d95f2010578fe5982f9b
ruheni <aleruheni@outlook.com>
2021-12-01 17:04:45 +0100

    chore: protect api routes using next-auth

--- typescript/rest-nextjs-api-routes-auth/pages/api/post/index.ts
+++ typescript/rest-nextjs-api-routes-auth/pages/api/post/index.ts
@@ -10,12 +10,16 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
   const { title, content } = req.body;
 
   const session = await getSession({ req });
-  const result = await prisma.post.create({
-    data: {
-      title: title,
-      content: content,
-      author: { connect: { email: session?.user?.email } },
-    },
-  });
-  res.json(result);
+  if (session) {
+    const result = await prisma.post.create({
+      data: {
+        title: title,
+        content: content,
+        author: { connect: { email: session?.user?.email } },
+      },
+    });
+    res.json(result);
+  } else {
+    res.status(401)
+  }
 }
