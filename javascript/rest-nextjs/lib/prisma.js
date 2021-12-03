96dcd267c00a0e1153f7f365c1d8a7a8b5906f3c
Aryan Beezadhur <34424160+AryanBeezadhur@users.noreply.github.com>
2021-12-03 19:27:05 +0000

    Update prisma.js
    Update JavaScript Next.js example so that hot reloading does not create new instances of PrismaClient.
    
    Code from https://github.com/prisma/prisma/issues/1983#issuecomment-620621213

--- javascript/rest-nextjs/lib/prisma.js
+++ javascript/rest-nextjs/lib/prisma.js
@@ -1,4 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-export default prisma;
+let prisma
+
+if (process.env.NODE_ENV === "production") {
+  // In production, create a new instance of PrismaClient
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    // In development, create a new global instance of PrismaClient to prevent hot reloading from creating new instances
+    global.prisma = new PrismaClient()
+  }
+
+  prisma = global.prisma
+}
+
+export default prisma
