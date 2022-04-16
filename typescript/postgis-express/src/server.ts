2eca6cc60fef9df0de93ec4efdc778ce6c674d65
Luis Mendes <machadoluismendes@gmail.com>
2022-04-16 19:24:43 -0300

    rush jsonwebtoken jwt

--- typescript/postgis-express/src/server.ts
+++ typescript/postgis-express/src/server.ts
@@ -2,7 +2,27 @@ import { PrismaClient } from '@prisma/client'
 import express from 'express'
 
 export const prisma = new PrismaClient()
+var cookieParser = require('cookie-parser')
+var csrf = require('csurf')
+var bodyParser = require('body-parser')
 const app = express()
+
+// setup route middlewares
+var csrfProtection = csrf({ cookie: true })
+var parseForm = bodyParser.urlencoded({ extended: false })
+
+// parse cookies
+// we need this because "cookie" is true in csrfProtection
+app.use(cookieParser())
+
+app.get('/form', csrfProtection, function (req, res) {
+  // pass the csrfToken to the view
+  res.render('send', { csrfToken: req.csrfToken() })
+})
+ 
+app.post('/process', parseForm, csrfProtection, function (req, res) {
+  res.send('data is being processed')
+})
 
 app.use(express.json())
 
