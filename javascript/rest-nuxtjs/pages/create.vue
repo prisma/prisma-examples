466ee0625e4f86ecd45dffcbd359bec67f5937b8
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2022-08-03 22:14:08 +0200

    chore: update urls to location.origin (#3979)

--- javascript/rest-nuxtjs/pages/create.vue
+++ javascript/rest-nuxtjs/pages/create.vue
@@ -29,7 +29,7 @@ export default {
       }
 
       try {
-        const res = await fetch(`http://localhost:3000/api/post`, {
+        const res = await fetch(`${location.origin}/api/post`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(body),
