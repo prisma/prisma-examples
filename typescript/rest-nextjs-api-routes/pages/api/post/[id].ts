94e135301dbdffee5d804161126ec1bdd3d8aed5
Christopher Fitzner <fitzner@cockroachlabs.com>
2022-08-23 01:56:12 -0700

    Remove serverside fetches from rest-nextjs-api-routes (#4015)

--- typescript/rest-nextjs-api-routes/pages/api/post/[id].ts
+++ typescript/rest-nextjs-api-routes/pages/api/post/[id].ts
@@ -1,28 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../lib/prisma'
 
-
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   const postId = req.query.id
 
-  if (req.method === 'GET') {
-    handleGET(postId, res)
-  } else if (req.method === 'DELETE') {
+  if (req.method === 'DELETE') {
     handleDELETE(postId, res)
   } else {
     throw new Error(
       `The HTTP ${req.method} method is not supported at this route.`
     )
   }
-}
-
-// GET /api/post/:id
-async function handleGET(postId, res) {
-  const post = await prisma.post.findUnique({
-    where: { id: Number(postId) },
-    include: { author: true },
-  })
-  res.json(post)
 }
 
 // DELETE /api/post/:id
