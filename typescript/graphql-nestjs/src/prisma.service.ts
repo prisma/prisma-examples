e5e08f8bea99e3e6307217eec970b1454c9d378e
ruheni <aleruheni@outlook.com>
2022-10-14 17:49:47 +0200

    chore: prettify examples

--- typescript/graphql-nestjs/src/prisma.service.ts
+++ typescript/graphql-nestjs/src/prisma.service.ts
@@ -2,16 +2,14 @@ import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
 
 @Injectable()
-export class PrismaService extends PrismaClient
-  implements OnModuleInit {
-
+export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    await this.$connect()
   }
 
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
-      await app.close();
-    });
+      await app.close()
+    })
   }
 }
