0d275fd1614e00252fdec7c56cde8bc0536b2bbb
Shantanu Sharma <shantanu34@outlook.com>
2022-10-14 16:03:33 +0530

    adding declare to remove TS warning for "global.prisma" (#4147)

--- typescript/rest-nextjs-api-routes-auth/lib/prisma.ts
+++ typescript/rest-nextjs-api-routes-auth/lib/prisma.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+declare var global: { prisma: PrismaClient };
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
