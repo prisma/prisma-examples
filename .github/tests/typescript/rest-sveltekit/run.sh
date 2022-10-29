fcf2aff086895e07d056103432143c13a71d4cb2
Omar Baabouj <baabouj.dev@gmail.com>
2022-10-29 22:31:48 +0100

    fix(.github/tests/typescript/rest-sveltekit/run.sh): load nvm before using it

--- .github/tests/typescript/rest-sveltekit/run.sh
+++ .github/tests/typescript/rest-sveltekit/run.sh
@@ -5,7 +5,9 @@ set -eu
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 
 # load the nvm environment
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 nvm install 16
 nvm use 16
