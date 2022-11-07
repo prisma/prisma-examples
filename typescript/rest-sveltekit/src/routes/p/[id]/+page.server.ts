5ac3503e84a32b6e1886a457e450dee005fb9b9a
Omar Baabouj <baabouj.dev@gmail.com>
2022-11-07 22:10:37 +0100

    chore(typescript/rest-sveltekit): use the provided fetch function instead of the native fetch web API

--- typescript/rest-sveltekit/src/routes/p/[id]/+page.server.ts
+++ typescript/rest-sveltekit/src/routes/p/[id]/+page.server.ts
@@ -1,8 +1,8 @@
 import type { Post } from "@prisma/client";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ url, params: { id } }) => {
-  const response = await fetch(`${url.origin}/api/post/${id}`);
+export const load: PageServerLoad = async ({ fetch, params: { id } }) => {
+  const response = await fetch(`/api/post/${id}`);
 
   return { post: (await response.json()) as Post };
 };
