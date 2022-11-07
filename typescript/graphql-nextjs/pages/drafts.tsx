6b32018542e9a2f53eed2bd7d650f61920e9d1e8
Karl Horky <karl.horky@gmail.com>
2022-11-07 14:18:02 +0100

    Simplify Next.js Link component usage

--- typescript/graphql-nextjs/pages/drafts.tsx
+++ typescript/graphql-nextjs/pages/drafts.tsx
@@ -4,20 +4,18 @@ import gql from "graphql-tag"
 import client from "../lib/apollo-client"
 
 const Post = ({ post }) => (
-  <Link href="/p/[id]" as={`/p/${post.id}`} legacyBehavior>
-    <a>
-      <h2>{post.title}</h2>
-      <small>By {post.author ? post.author.name : "Unknown Author"}</small>
-      <p>{post.content}</p>
-      <style jsx>{`
-        a {
-          text-decoration: none;
-          color: inherit;
-          padding: 2rem;
-          display: block;
-        }
-      `}</style>
-    </a>
+  <Link href={`/p/${post.id}`}>
+    <h2>{post.title}</h2>
+    <small>By {post.author ? post.author.name : "Unknown Author"}</small>
+    <p>{post.content}</p>
+    <style jsx>{`
+      a {
+        text-decoration: none;
+        color: inherit;
+        padding: 2rem;
+        display: block;
+      }
+    `}</style>
   </Link>
 )
 
