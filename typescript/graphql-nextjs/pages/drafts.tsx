409bc860181a5b3d603e900bd544541a01f86e87
Karl Horky <karl.horky@gmail.com>
2022-11-07 14:49:54 +0100

    Move styles

--- typescript/graphql-nextjs/pages/drafts.tsx
+++ typescript/graphql-nextjs/pages/drafts.tsx
@@ -8,14 +8,6 @@ const Post = ({ post }) => (
     <h2>{post.title}</h2>
     <small>By {post.author ? post.author.name : "Unknown Author"}</small>
     <p>{post.content}</p>
-    <style jsx>{`
-      a {
-        text-decoration: none;
-        color: inherit;
-        padding: 2rem;
-        display: block;
-      }
-    `}</style>
   </Link>
 )
 
@@ -44,6 +36,13 @@ const Drafts = (props) => {
 
         .post + .post {
           margin-top: 2rem;
+        }
+
+        .post a {
+          text-decoration: none;
+          color: inherit;
+          padding: 2rem;
+          display: block;
         }
       `}</style>
     </Layout>
