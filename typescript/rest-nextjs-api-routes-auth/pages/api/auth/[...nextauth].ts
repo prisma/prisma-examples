e6bc773a41e84520a83b51673e176f57ffd31dc7
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2023-01-12 10:30:29 +0000

    fix(example): Add typing and remove unused imports (#4363)

--- typescript/rest-nextjs-api-routes-auth/pages/api/auth/[...nextauth].ts
+++ typescript/rest-nextjs-api-routes-auth/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,14 @@
 import { NextApiHandler } from "next";
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import GitHubProvider from 'next-auth/providers/github'
-import EmailProvider from 'next-auth/providers/email'
-import prisma from '../../../lib/prisma'
-
+import NextAuth, { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GitHubProvider from "next-auth/providers/github";
+import EmailProvider from "next-auth/providers/email";
+import prisma from "../../../lib/prisma";
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
 
-const options = {
+const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
