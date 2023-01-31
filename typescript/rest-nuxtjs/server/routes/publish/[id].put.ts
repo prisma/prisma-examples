90d11adf36cce20ff98f9671c5aeb8207d6dab70
Anthony Jekanyika <anthonyjek1950@gmail.com>
2023-01-31 14:11:57 +0200

    Fixed bug

--- typescript/rest-nuxtjs/server/routes/publish/[id].put.ts
+++ typescript/rest-nuxtjs/server/routes/publish/[id].put.ts
@@ -2,7 +2,7 @@ import { prisma } from '../../../prisma/db'
 
 // https://nuxt.com/docs/guide/directory-structure/server
 export default defineEventHandler(async (event) => {
-    const id = event.context.params;
+    const id = event.context.params.id;
 
     const updatePost = await prisma.post.update({
         where: {
