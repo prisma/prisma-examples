a234e56b6f5e039e76c251588c8d548706e41182
Eng Zer Jun <engzerjun@gmail.com>
2023-02-13 22:16:25 +0800

    Update Next.js examples
    Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

--- javascript/rest-nextjs/lib/prisma.js
+++ javascript/rest-nextjs/lib/prisma.js
@@ -1,4 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global;
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 export default prisma;
--- typescript/rest-nextjs-api-routes/src/lib/prisma.ts
+++ typescript/rest-nextjs-api-routes/src/lib/prisma.ts
@@ -12,12 +12,8 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  })
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export default prisma;
+export default prisma
