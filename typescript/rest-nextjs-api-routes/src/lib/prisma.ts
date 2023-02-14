a62ca17c608b0dba002fbef01548af4407b84e0b
Eng Zer Jun <engzerjun@gmail.com>
2023-02-15 00:44:40 +0800

    feat: update `prisma.ts` in Next.js examples (#4488)

--- typescript/rest-nextjs-api-routes/src/lib/prisma.ts
+++ typescript/rest-nextjs-api-routes/src/lib/prisma.ts
@@ -10,21 +10,10 @@ import { PrismaClient } from '@prisma/client'
 // Learn more:
 // https://pris.ly/d/help/next-js-best-practices
 
-declare global {
-  var prisma: PrismaClient;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-let prisma: PrismaClient
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  // @ts-ignore 7017
-  if (!global.prisma) {
-    // @ts-ignore 7017
-    global.prisma = new PrismaClient()
-  }
-  // @ts-ignore 7017
-  prisma = global.prisma
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
 export default prisma
--- typescript/trpc-nextjs/src/server/db.ts
+++ typescript/trpc-nextjs/src/server/db.ts
@@ -2,15 +2,8 @@ import { PrismaClient } from "@prisma/client";
 
 import { env } from "../env/server.mjs";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (env.NODE_ENV !== "production") {
-  global.prisma = prisma;
-}
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
