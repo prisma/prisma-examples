4d0c7a28071b100d5bcfafffe23f43a2b2beab28
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2023-07-12 09:17:21 -0500

    Apply suggestions from code review
    Co-authored-by: Joël Galeran <Jolg42@users.noreply.github.com>

--- typescript/rest-nestjs/src/prisma.service.ts
+++ typescript/rest-nestjs/src/prisma.service.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from '@prisma/client'
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
+		// Note: this is optional
     await this.$connect()
   }
 }
