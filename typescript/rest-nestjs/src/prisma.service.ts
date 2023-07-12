fbfe21a9649cf288dd392be876f58ed8ce220af0
Joël Galeran <Jolg42@users.noreply.github.com>
2023-07-12 16:18:08 +0200

    Update typescript/rest-nestjs/src/prisma.service.ts

--- typescript/rest-nestjs/src/prisma.service.ts
+++ typescript/rest-nestjs/src/prisma.service.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client'
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-		// Note: this is optional
+    // Note: this is optional
     await this.$connect()
   }
 }
