8cfbf67a2bfb69e2394659b81ebe18575dbb404c
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2023-07-12 09:55:54 +0000

    remove nvm use for sveltekit examples

--- .github/tests/javascript/rest-sveltekit/run.sh
+++ .github/tests/javascript/rest-sveltekit/run.sh
@@ -2,16 +2,6 @@
 
 set -eu
 
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-
-# load the nvm environment
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-nvm install 16
-nvm use 16
-
 npm install 
 npx prisma migrate dev --name init
 npm run dev &
