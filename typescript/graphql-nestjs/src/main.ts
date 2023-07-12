efbbf8da1f24f52b13289baf4164fe6205b09282
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2023-07-12 09:13:38 -0500

    Update NestJS examples to no longer use beforeExit hook Fixes #4931

--- typescript/graphql-nestjs/src/main.ts
+++ typescript/graphql-nestjs/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableShutdownHooks()
   await app.listen(3000, () => {
     console.log(`
 🚀 Server ready at: http://localhost:3000/graphql
--- typescript/rest-nestjs/src/main.ts
+++ typescript/rest-nestjs/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableShutdownHooks()
   await app.listen(3000)
 }
 bootstrap()
