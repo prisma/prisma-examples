43d7bd98bf1148479bcc82a39cc2cd7791c8826c
ATMedia <bullcherry@naver.com>
2023-09-14 14:59:59 +0900

    fix CLIENT_FETCH_ERROR that occurs when creating a new draft from the /create route.

--- typescript/rest-nextjs-api-routes-auth/pages/api/post/index.ts
+++ typescript/rest-nextjs-api-routes-auth/pages/api/post/index.ts
@@ -1,15 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../lib/prisma'
-import { getSession } from 'next-auth/react'
-
+import { getServerSession } from 'next-auth/next'
+import { options } from '../auth/[...nextauth]'
 
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const { title, content } = req.body;
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { title, content } = req.body
 
-  const session = await getSession({ req });
+  const session = await getServerSession(req, res, options)
   if (session) {
     const result = await prisma.post.create({
       data: {
@@ -17,8 +20,8 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
         content: content,
         author: { connect: { email: session?.user?.email } },
       },
-    });
-    res.json(result);
+    })
+    res.json(result)
   } else {
     res.status(401).send({ message: 'Unauthorized' })
   }
