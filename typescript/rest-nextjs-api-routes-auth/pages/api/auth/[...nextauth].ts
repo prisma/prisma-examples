b43610faba489cff03da73e2f757f9db9715fdc8
bullcherry <85793301+bullcherry@users.noreply.github.com>
2023-12-19 00:25:21 +0900

    Fix: rest-nextjs-api-routes-auth CLIENT_FETCH_ERROR #4733 (#5233)
    * fix CLIENT_FETCH_ERROR that occurs when creating a new draft from the /create route.
    
    * few style tweaks
    
    ---------
    
    Co-authored-by: Jon Harrell <4829245+jharrell@users.noreply.github.com>

--- typescript/rest-nextjs-api-routes-auth/pages/api/auth/[...nextauth].ts
+++ typescript/rest-nextjs-api-routes-auth/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,14 @@
-import { NextApiHandler } from "next";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import GitHubProvider from "next-auth/providers/github";
-import EmailProvider from "next-auth/providers/email";
-import prisma from "../../../lib/prisma";
+import { NextApiHandler } from 'next';
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GitHubProvider from 'next-auth/providers/github';
+import EmailProvider from 'next-auth/providers/email';
+import prisma from '../../../lib/prisma';
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
 
-const options: NextAuthOptions = {
+export const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
