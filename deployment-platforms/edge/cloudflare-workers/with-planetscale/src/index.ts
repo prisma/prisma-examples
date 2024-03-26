7cdbba4f602a886e04427dd7c064aa668b4ad67e
Nikolas Burk <nikolas.burk@gmail.com>
2024-03-26 16:44:17 +0100

    add d1 example

--- deployment-platforms/edge/cloudflare-workers/with-planetscale/src/index.ts
+++ deployment-platforms/edge/cloudflare-workers/with-planetscale/src/index.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaPlanetScale } from '@prisma/adapter-planetscale'
 import { Client } from '@planetscale/database'
+import { Request. ExecutionContext} from "@cloudflare/workers-types"
+
+type Env = {
+	DATABASE_URL: string
+}
 
 export default {
-  async fetch(request, env, ctx) {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
     const client = new Client({
       url: env.DATABASE_URL,
       // see https://github.com/cloudflare/workerd/issues/698
