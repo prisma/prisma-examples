9c1aa166873658534af66f4482a7e1ef948d02dd
Ankur Datta <datta@prisma.io>
2024-05-28 15:04:50 +0600

    enhance: update app text copy

--- pulse/product-search-with-typesense/frontend/app/page.tsx
+++ pulse/product-search-with-typesense/frontend/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex flex-col items-center">
       <div className="text-center pt-16 pl-16 pr-16">
         <h1 className="text-2xl font-bold">
-          Welcome to Typesense Cloudflare product search 🔎
+          Welcome to the Typesense + Prisma Pulse product search exmple app 🔎
         </h1>
         <p className="p-4">
         This app lets you add products to your database and automatically syncs them with the TypeSense server every five minutes using Prisma Pulse, allowing you to search for the added products easily.
