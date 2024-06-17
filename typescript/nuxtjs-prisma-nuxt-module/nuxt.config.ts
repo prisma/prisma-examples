80db8c7862a962244d3984919aa3337c7c3fa6ef
Ankur Datta <datta@prisma.io>
2024-06-17 21:52:14 +0600

    enhance: improve clarity of example docs

--- typescript/nuxtjs-prisma-nuxt-module/nuxt.config.ts
+++ typescript/nuxtjs-prisma-nuxt-module/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@prisma/nuxt'],
+  // You can learn more about the `@prisma/nuxt` module configuration options [here](https://pris.ly/configure-prisma-nuxt)
   prisma: {
     autoSetupPrisma: true,
   },
