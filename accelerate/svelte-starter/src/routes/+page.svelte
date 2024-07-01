5952fe372fd1f15c8c67f4e61ee391ccd17ecae6
ludralph <raphaelumoh@gmail.com>
2024-07-01 11:08:24 +0100

    feat: add refresh button

--- accelerate/svelte-starter/src/routes/+page.svelte
+++ accelerate/svelte-starter/src/routes/+page.svelte
@@ -31,6 +31,10 @@
       console.error('Failed to fetch quotes:', error)
     }
   })
+
+  const refreshPage = () => {
+    window.location.reload();
+  };
 </script>
 
 <main class="flex min-h-screen flex-col items-center justify-start p-8 md:p-24">
@@ -38,7 +42,9 @@
   <p class="text-xl text-slate-800">
     Retrieves the most recently added quote with and without caching enabled
   </p>
+  <button class="mt-9 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" on:click={refreshPage}>Refresh Page</button>
   <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-9">
+    
     <Quote title="Cached Quote" type="TTL" result={ttl} />
     <Quote title="Cached Quote" type="SWR" result={swr} />
     <Quote title="Cached Quote" type="TTL + SWR" result={both} />
