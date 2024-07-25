5e2920174e9b2f439398c4beab8e6b18445ba8ba
Ankur Datta <datta@prisma.io>
2024-07-25 18:41:27 +0545

    fix: remove unecessary code

--- optimize/starter/utils/db.ts
+++ optimize/starter/utils/db.ts
@@ -1,19 +1,4 @@
 import { PrismaClient } from '@prisma/client'
 import { withOptimize } from '@prisma/extension-optimize'
 
-export const prisma = new PrismaClient().$extends(
-  withOptimize({
-    dashboardUrl: 'https://optimize-dev-exp-182-detect.prisma.workers.dev',
-    ingestionUrl:
-      'https://optimize-ingestion-dev-exp-182-detect.prisma.workers.dev',
-  }),
-)
-
-// .$extends(
-//   withOptimize({
-//     dashboardUrl: "https://optimize-dev-dev.prisma.workers.dev/",
-//     ingestionUrl: "https://optimize-ingestion-dev-dev.prisma.workers.dev/",
-//   })
-// );
-
-// https://optimize-dev-exp-182-detect.prisma.workers.dev
+export const prisma = new PrismaClient().$extends(withOptimize({}))
