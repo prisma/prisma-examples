6205888ff2949db7fe1acab8c65039888961437e
Shane Neubauer <shanen@gmail.com>
2024-08-19 17:06:06 +0100

    Add a write operation to the TypeScript starter example

--- typescript/starter/src/index.ts
+++ typescript/starter/src/index.ts
@@ -2,6 +2,14 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
+  const newUser = await prisma.user.create({
+    data: {
+      name: 'Percy Prisma',
+      email: 'percy@example.com',
+    },
+  });
+  console.log('The new user:', newUser);
+
   const users = await prisma.user.findMany();
   console.log('All users:', users);
 
