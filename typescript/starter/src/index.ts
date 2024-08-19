1d5fa867b637758a4af58aef7a41afcfc29d4bea
Shane Neubauer <shanen@gmail.com>
2024-08-19 16:28:23 +0100

    Apply suggestions from code review
    Co-authored-by: Jon Harrell <4829245+jharrell@users.noreply.github.com>

--- typescript/starter/src/index.ts
+++ typescript/starter/src/index.ts
@@ -5,7 +5,7 @@ async function main() {
   const users = await prisma.user.findMany();
   console.log('All users:', users);
 
-  // See more examples of how to use the Prisma ORM: https://www.prisma.io/docs/getting-started/quickstart
+  // See more examples of how to use Prisma ORM: https://www.prisma.io/docs/getting-started/quickstart
 }
 
 main();
