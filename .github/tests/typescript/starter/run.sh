2061eede08f669344fee77dc5d0e1b825fca9923
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2024-08-21 05:39:00 +0600

    Revert "Merge branch 'latest' into optimize-example"
    This reverts commit b574aa6c3c87dd10757a163d90284852a8f15d51, reversing
    changes made to 8eaa1036e89d0bf965c56d844e79ec9606cdf9c5.

--- .github/tests/typescript/starter/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-npm install
-npm run build
--- optimize/starter/packages/server/src/router/index.ts
+++
@@ -1,8 +0,0 @@
-import { trpc } from '../lib/trpc'
-import { todoRouter } from './todoRouter'
-
-export const appRouter = trpc.router({
-  todo: todoRouter,
-})
-
-export type AppRouter = typeof appRouter
