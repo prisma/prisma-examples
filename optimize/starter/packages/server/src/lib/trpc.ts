768a66361a59817b1512994a622319bd6c2eafc0
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-09-04 10:59:28 -0500

    Optimize starter fixes/updates (#6588)
    * Apply suggestions from code review
    
    * further tweaks

--- optimize/starter/packages/server/src/lib/trpc.ts
+++ optimize/starter/packages/server/src/lib/trpc.ts
@@ -1,11 +1,13 @@
-import { inferAsyncReturnType, initTRPC } from "@trpc/server";
-import * as trpcExpress from "@trpc/server/adapters/express";
+import { initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
 
-export const createContext = (
-  _options: trpcExpress.CreateExpressContextOptions
-) => {
-  return {};
-};
+export const createContext = ({
+  _req,
+  _res,
+}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+type Context = Awaited<ReturnType<typeof createContext>>;
 
-type Context = inferAsyncReturnType<typeof createContext>;
-export const trpc = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create();
+
+export const router = t.router;
+export const publicProcedure = t.procedure;
