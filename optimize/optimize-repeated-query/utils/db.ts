4132ed2f189331bd1a997e8f033749c666b85b08
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2024-10-14 19:17:15 +0600

    test: check url

--- optimize/optimize-repeated-query/utils/db.ts
+++ optimize/optimize-repeated-query/utils/db.ts
@@ -3,6 +3,8 @@ import { withOptimize } from '@prisma/extension-optimize'
 
 export const prisma = new PrismaClient().$extends(
   withOptimize({
+    dashboardUrl: "https://optimize-dev-pr606.prisma.workers.dev",
+    ingestionUrl: "https://optimize-ingestion-dev-pr606.prisma.workers.dev",
     apiKey: process.env.OPTIMIZE_API_KEY!,
   }),
 )
