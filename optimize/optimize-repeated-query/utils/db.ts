9dbff8c79f98d24f5c8d7fca1ee4696369370104
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2024-10-18 19:40:49 +0600

    patch: update db.ts

--- optimize/optimize-repeated-query/utils/db.ts
+++ optimize/optimize-repeated-query/utils/db.ts
@@ -3,8 +3,6 @@ import { withOptimize } from '@prisma/extension-optimize'
 
 export const prisma = new PrismaClient().$extends(
   withOptimize({
-    dashboardUrl: "https://optimize-dev-pr606.prisma.workers.dev",
-    ingestionUrl: "https://optimize-ingestion-dev-pr606.prisma.workers.dev",
-    apiKey: process.env.OPTIMIZE_API_KEY!,
+    apiKey: process.env.OPTIMIZE_API_KEY!
   }),
 )
