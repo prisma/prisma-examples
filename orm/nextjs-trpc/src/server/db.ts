7163bec92e636ca7248f3f481e313f9c08b20937
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2025-01-17 11:11:31 -0600

    remove old examples

--- orm/nextjs-trpc/src/server/db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-import { env } from "../env/server.mjs";
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
