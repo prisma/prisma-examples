5abcd7fa84982733249b58d61143908e2b15ff6c
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2025-02-19 14:52:55 +0600

    chore: add typedsql to the examples

--- orm/typedsql/src/index.ts
+++ orm/typedsql/src/index.ts
@@ -1,23 +1,25 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 import { conversionByVariant } from '@prisma/client/sql'
 import { filterTrackingEvents } from '@prisma/client/sql'
 import { getTrackingEvents } from '@prisma/client/sql'
 
 async function main() {
-  const prisma = new PrismaClient()
+  const prisma = new PrismaClient().$extends(withAccelerate())
 
   const stats = await prisma.$queryRawTyped(conversionByVariant())
   console.log(stats)
 
-  const rows = await prisma.$queryRawTyped(filterTrackingEvents(
-    JSON.stringify(['PageOpened', 'ButtonClicked']),
-    JSON.stringify(['BlueBuyButton', 'RedBuyButton'])
-  ))
+  const rows = await prisma.$queryRawTyped(
+    filterTrackingEvents(
+      JSON.stringify(['PageOpened', 'ButtonClicked']),
+      JSON.stringify(['BlueBuyButton', 'RedBuyButton']),
+    ),
+  )
   console.log(rows)
 
   const result = await prisma.$queryRawTyped(getTrackingEvents(5))
   console.log(result)
- 
 }
 
 main()
