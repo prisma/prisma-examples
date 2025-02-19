190e116d37836905f3f11222b22078d402490c9d
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2025-02-19 14:52:55 +0600

    resolve: resolve conflict for typedsql

--- orm/typedsql/src/index.ts
+++ orm/typedsql/src/index.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 import { conversionByVariant } from '@prisma/client/sql'
 import { filterTrackingEvents } from '@prisma/client/sql'
 import { getTrackingEvents } from '@prisma/client/sql'
 
 async function main() {
-  const prisma = new PrismaClient()
+  const prisma = new PrismaClient().$extends(withAccelerate())
 
   const stats = await prisma.$queryRawTyped(conversionByVariant())
   console.log(stats)
