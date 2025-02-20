18585d0a928de3a5599da1d57d3bd3f04c2b25f8
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2025-02-20 13:25:44 +0600

    fix: add database url

--- .github/tests/orm/testing-express/run.sh
+++ .github/tests/orm/testing-express/run.sh
@@ -2,5 +2,11 @@
 
 set -eu
 
+# Check if DATABASE_URL is set
+if [ -z "${PPG_TEST_DATABASE_URL:-}" ]; then
+  echo "Error: DATABASE_URL environment variable is not set."
+  exit 1
+fi
+
 npm install
 npm run test
