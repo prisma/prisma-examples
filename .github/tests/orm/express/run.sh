181b09a1b8efa7b2ef5abcc73cd67c70cb0c24aa
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2025-02-20 15:51:43 +0600

    fix: add seeders to tests

--- .github/tests/orm/express/run.sh
+++ .github/tests/orm/express/run.sh
@@ -7,11 +7,12 @@ export DATABASE_URL="${PPG_TEST_DATABASE_URL}"
 npm install
 npx prisma migrate reset --force --skip-seed
 npx prisma migrate dev --name init
+npx prisma db seed
 npm run dev &
 pid=$!
 
 sleep 20
 
-curl --fail 'http://localhost:3000/'
+npx newman run ../../.github/tests/postman_collections/rest.json --bail
 
 kill "$pid"
