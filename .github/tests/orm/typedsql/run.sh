ae4e03c8460b55e0e25acf4e3f5c32ff07a4757f
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2025-02-20 17:40:15 +0600

    fix: typedsql tests

--- .github/tests/orm/typedsql/run.sh
+++ .github/tests/orm/typedsql/run.sh
@@ -2,11 +2,8 @@
 
 set -eu
 
-export DATABASE_URL="${PPG_TEST_DATABASE_URL}"
-
 npm install
-npx prisma migrate reset --force --skip-seed
 npx prisma migrate dev --name init
-npx prisma generate --sql --no-engine
+npx prisma generate --sql
 npx prisma db seed
-npm run dev
+npm run start
