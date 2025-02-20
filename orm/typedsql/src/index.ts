08fa973c2e02a3c0b507d6a6756807948a46a9a5
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2025-02-20 17:40:15 +0600

    resolve: typedsql package

--- orm/typedsql/src/index.ts
+++ orm/typedsql/src/index.ts
@@ -1,11 +1,10 @@
 import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
 import { conversionByVariant } from '@prisma/client/sql'
 import { filterTrackingEvents } from '@prisma/client/sql'
 import { getTrackingEvents } from '@prisma/client/sql'
 
 async function main() {
-  const prisma = new PrismaClient().$extends(withAccelerate())
+  const prisma = new PrismaClient()
 
   const stats = await prisma.$queryRawTyped(conversionByVariant())
   console.log(stats)
