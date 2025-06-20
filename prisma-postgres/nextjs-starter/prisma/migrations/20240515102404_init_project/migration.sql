d496e9d00760507b86a42b9f7778ee8c10c6f661
jkomyno <12381818+jkomyno@users.noreply.github.com>
2025-06-20 15:56:28 +0400

    feat(prisma-postgres): update prisma schema and version, using the "prisma-client" generator

--- prisma-postgres/nextjs-starter/prisma/migrations/20240515102404_init_project/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Quotes" (
-    "id" SERIAL NOT NULL,
-    "quote" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Quotes_pkey" PRIMARY KEY ("id")
-);
