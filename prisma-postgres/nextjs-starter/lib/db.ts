528e22c8f445747164be690ef275efce5c257638
jkomyno <12381818+jkomyno@users.noreply.github.com>
2025-06-20 16:01:13 +0400

    feat(prisma-postgres): add getDb utility wrapping Prisma Driver Adapters

--- prisma-postgres/nextjs-starter/lib/db.ts
+++ prisma-postgres/nextjs-starter/lib/db.ts
@@ -1,18 +1,19 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from './.generated/prisma/client';
+import { env } from './env/server'
 
-// Learn more about instantiating PrismaClient in Next.js here: https://www.prisma.io/docs/data-platform/accelerate/getting-started
+export { QuoteKind } from './.generated/prisma/enums'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient().$extends(withAccelerate());
-};
+export type GetDbParams = {
+  connectionString: string
+}
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+export function getDb({ connectionString }: GetDbParams) {
+  const pool = new PrismaPg({ connectionString })
+  const prisma = new PrismaClient({ adapter: pool })
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+  return prisma
+}
 
-export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+const prisma = getDb({ connectionString: env.DATABASE_URL })
+export default prisma
