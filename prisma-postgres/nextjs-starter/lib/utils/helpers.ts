e8d86feabefb16f0b828ec5f6716f1f494f70e06
jkomyno <12381818+jkomyno@users.noreply.github.com>
2025-06-23 16:41:30 +0400

    feat(prisma-postgres): rename nextjs-starter project to nextjs-starter-webpack

--- prisma-postgres/nextjs-starter/lib/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
-export async function delay(ms: number): Promise<void> {
-  return new Promise<void>((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, ms)
-  })
-}
-
-export const HOST = process.env.NEXT_PUBLIC_URL ?? 'http://localhost:3000'
