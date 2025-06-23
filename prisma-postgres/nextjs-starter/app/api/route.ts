33ca098abee9cfdb2858e3aa960fe2d75d4a8ebc
jkomyno <12381818+jkomyno@users.noreply.github.com>
2025-06-23 15:59:27 +0400

    feat(prisma-postgres): got working example with [patched] prisma-client, enums on client side

--- prisma-postgres/nextjs-starter/app/api/route.ts
+++ prisma-postgres/nextjs-starter/app/api/route.ts
@@ -1,65 +1,26 @@
-import { addQuote, getQuotes } from "@/lib/utils/query";
-import { z } from "zod";
+import { getQuotes } from '@/lib/utils/query'
 
-export const runtime = "edge";
+/**
+ * `runtime = "edge"` would cause issues such as:
+ *   2 |
+ *   3 | var path = require('path')
+ * > 4 |   , fs = require('fs')
+ *     | ^
+ *   5 |   , helper = require('./helper.js')
+ *   6 | ;
+ *   7 |
+ */
+// export const runtime = "edge";
 
 // disabling caching
-export const fetchCache = "force-no-store";
-export const revalidate = 0;
+export const fetchCache = 'force-no-store'
+export const revalidate = 0
 
-export async function GET(request: Request) {
-  const url = new URL(request.url);
-  const searchParams = new URLSearchParams(url.search);
-
-  const parser = z.enum(["TTL", "SWR", "BOTH", "NONE"]);
-  const parsedOutput = await parser.safeParseAsync(searchParams.get("cache"));
-
-  if (!parsedOutput.success) {
-    return new Response(JSON.stringify("Invalid search parameter."), {
-      status: 400,
-    });
-  }
-
-  let map = new Map();
-
-  // When TTL is selected
-  map.set("TTL", {
-    ttl: 30,
-  });
-
-  // When SWR is selected
-  map.set("SWR", {
-    swr: 30,
-  });
-
-  // When TTL + SWR is selected
-  map.set("BOTH", {
-    ttl: 30,
-    swr: 60,
-  });
-
-  // This ensures no caching is performed and only the Accelerate connection pool is used
-  map.set("NONE", undefined);
-
-  const data = await getQuotes(map.get(parsedOutput.data));
+export async function GET(_request: Request) {
+  const data = await getQuotes()
 
   return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" },
+    headers: { 'Content-Type': 'application/json' },
     status: 200,
-  });
-}
-
-export async function POST(request: Request) {
-  const response = await fetch("https://api.quotable.io/random", {
-    cache: "no-cache",
-  });
-
-  const data = await response.json();
-
-  await addQuote(data.content);
-
-  return new Response(JSON.stringify({ quote: data.content }), {
-    headers: { "Content-Type": "application/json" },
-    status: 201,
-  });
+  })
 }
--- prisma-postgres/nextjs-starter/app/page.tsx
+++ prisma-postgres/nextjs-starter/app/page.tsx
@@ -1,25 +1,24 @@
-import { AddQuote } from '@/components/Quote/AddQuote'
-import { QuotesLoading } from '@/components/Quote/QuotesLoading'
-import { Quotes } from '@/components/Quote/Quotes'
 import { Suspense } from 'react'
-
-// Disable caching. If 'force-dynamic' is not used, stale data can be returned from Prisma Client.
-// Learn more here: https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dynamic.
-export const dynamic = 'force-dynamic'
+import { Quotes } from '@/components/quotes'
+import { QuotesLoading } from '@/components/quotes-loading'
+import { DisplayPrismaEnums } from '@/components/display-prisma-enums'
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-8 md:p-24">
-      <h1 className="text-3xl text-slate-800 dark:text-white">
-        Accelerated Quotes
-      </h1>
-      <br />
-      <p className="text-xl text-slate-800 dark:text-slate-200">
-        Retrieves the most recently added quote with and without caching enabled
-      </p>
-      <br />
-      <AddQuote />
-      <br />
+    <main className="flex min-h-screen flex-col items-center justify-start p-8 md:p-24 bg-white dark:bg-slate-900">
+      <div className="text-center mb-12">
+        <h1 className="text-4xl font-bold text-slate-800 dark:text-white mb-4">
+          Quotes from Prisma Postgres
+        </h1>
+
+        <p className="text-xl text-slate-600 dark:text-slate-300">
+          A collection of inspiring opinions and fascinating facts
+        </p>
+      </div>
+      <div className="text-center mb-12">
+        <DisplayPrismaEnums />
+      </div>
+
       <Suspense fallback={<QuotesLoading />}>
         <Quotes />
       </Suspense>
