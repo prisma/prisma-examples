c5ee47483745c80a2bdfc28e8200374f3da1cf1c
jkomyno <12381818+jkomyno@users.noreply.github.com>
2025-06-26 18:55:28 +0400

    feat(prisma-client): new nextjs-starter-webpack example, with Next.js 15, Webpack, Node.js, Prisma Postgres

---
+++ prisma-postgres/nextjs-starter-webpack/tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {},
+  plugins: [],
+}
+export default config
