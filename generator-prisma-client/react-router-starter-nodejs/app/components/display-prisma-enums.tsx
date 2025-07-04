e131c88428dfbbeacc71c3a5d1c4597cf94589fa
jkomyno <12381818+jkomyno@users.noreply.github.com>
2025-07-04 16:15:08 +0400

    feat(generator-prisma-client): add React Router + Node.js example

---
+++ generator-prisma-client/react-router-starter-nodejs/app/components/display-prisma-enums.tsx
@@ -0,0 +1,30 @@
+import { QuoteKind } from '~/generated/prisma/enums'
+import { useState, useEffect } from 'react'
+
+export function DisplayPrismaEnums() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    // Note: this avoid "Hydration failed" errors
+    setMounted(true)
+  }, [])
+
+  return (
+    <div className="w-full max-w-2xl mx-auto">
+      <div className="border-4 border-dashed border-red-400 bg-yellow-50 dark:bg-yellow-900/30 rounded-lg p-4 text-xs text-yellow-800 dark:text-yellow-200 shadow-sm">
+        <div className="mb-2 font-semibold text-center">
+          Debug Info: Prisma Enums (Client Side)
+        </div>
+        <div className="mb-1 text-center">
+          Rendered on client?{' '}
+          <span className="font-mono">{mounted ? 'Yes' : 'No'}</span>
+        </div>
+        <div className="mb-2 text-center">
+          <span className="font-semibold">
+            QuoteKind Enum: {QuoteKind.Fact}; {QuoteKind.Opinion}
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
