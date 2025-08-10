14550492fc2c9ef476f048d3219f6fd1089283c2
jkomyno <12381818+jkomyno@users.noreply.github.com>
2025-08-11 02:10:26 +0300

    fix(generator-prisma-client): fix deno

--- generator-prisma-client/deno-deploy/prisma.config.ts
+++ generator-prisma-client/deno-deploy/prisma.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'prisma/config'
 
+// Note: this wouldn't be needed if `deno task --env-file=.env ...` was supported.
+// See: https://github.com/denoland/deno/issues/27236 
+import 'dotenv/config'
+
 export default defineConfig({
   schema: './prisma/schema.prisma',
   migrations: {
     path: './prisma/migrations',
-    seed: 'deno run --allow-all --env-file=.env ./prisma/seed.ts',
+    seed: 'deno run --allow-all ./prisma/seed.ts',
   },
 })
