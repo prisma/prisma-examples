39f29604d4410dfab84ea31fa088c7287e3ae3f0
Tobias Diez <code@tobiasdiez.de>
2025-08-19 12:41:13 +0800

    Nuxt, generator-prisma-client: Use experimental wasm mode of nitro

--- generator-prisma-client/nuxt3-starter-nodejs/nuxt.config.ts
+++ generator-prisma-client/nuxt3-starter-nodejs/nuxt.config.ts
@@ -3,5 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  nitro: {
+    experimental: {
+      wasm: true,
+    },
+  },
 })
