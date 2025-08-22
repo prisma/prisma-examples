06c0a67b14e97b80a86efc6fde17d42f57b0425d
jkomyno <12381818+jkomyno@users.noreply.github.com>
2025-08-22 15:07:00 +0200

    feat(generator-prisma-client): minor fixes

--- generator-prisma-client/vitest-esm-nodejs/test/test-setup.ts
+++ generator-prisma-client/vitest-esm-nodejs/test/test-setup.ts
@@ -2,7 +2,7 @@ import { execa } from 'execa'
 
 export async function setup() {
   console.log('Running Prisma DB push...')
-  await execa('pnpm', ['prisma', 'db', 'push'], {
+  await execa('pnpm', ['prisma', 'db', 'push', '--force-reset', '--skip-generate'], {
     stdio: 'inherit',
     cwd: process.cwd(),
   })
