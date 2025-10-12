9fd8d7cd08dfca4e5c0e50467e59ea01255ee94f
Rama Aditya <ramaaditya4902@gmail.com>
2025-10-12 18:42:07 +0700

    Update db.ts
    use prisma for global variable on server side

--- generator-prisma-client/nuxt4-starter-nodejs/server/db.ts
+++ generator-prisma-client/nuxt4-starter-nodejs/server/db.ts
@@ -1,16 +1,19 @@
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../shared/generated/prisma/client'
+import { PrismaClient } from '#shared/generated/prisma/client'
 
-export type GetDbParams = {
-  connectionString: string
+// Global database instance for server-side usage
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-export function getDb({ connectionString }: GetDbParams) {
-  const pool = new PrismaPg({ connectionString })
-  const prisma = new PrismaClient({ adapter: pool })
+export const prisma =
+  globalForPrisma.prisma ??
+  (() => {
+    const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+    return new PrismaClient({ adapter: pool })
+  })()
 
-  return prisma
-}
+// Store in global for both development and production
+globalForPrisma.prisma = prisma
 
-const prisma = getDb({ connectionString: process.env.DIRECT_URL! })
 export default prisma
