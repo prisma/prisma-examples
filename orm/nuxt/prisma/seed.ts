3d48eb5008ee1d50811213e8189c0d155e4d79f8
Aman Varshney <amanvarshney.work@gmail.com>
2025-10-12 15:44:26 +0530

    test

--- orm/nuxt/prisma/seed.ts
+++ orm/nuxt/prisma/seed.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from '../prisma/generated/prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from './generated/client'
 
-const prisma = new PrismaClient().$extends(withAccelerate())
+const prisma = new PrismaClient()
 
 const userData = [
   {
