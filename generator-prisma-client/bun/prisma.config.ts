0b9cd7478a1dc89eb87431e2e8696d6f5ff5623c
Aman Varshney <amanvarshney.work@gmail.com>
2025-11-01 00:15:31 +0530

    update

---
+++ generator-prisma-client/bun/prisma.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+    schema: "prisma/schema.prisma",
+    migrations: {
+        path: "prisma/migrations",
+    },
+    engine: "classic",
+    datasource: {
+        url: env("DATABASE_URL"),
+    },
+});
