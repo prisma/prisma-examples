d3dfe2faa7836dee3f9c1fd456e3a53f7f80011d
Aman Varshney <amanvarshney.work@gmail.com>
2025-11-11 00:25:34 +0530

    update nextjs-starter

--- accelerate/nextjs-starter/lib/db.ts
+++ accelerate/nextjs-starter/lib/db.ts
@@ -1,10 +1,11 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "../prisma/generated/client";
 
 // Learn more about instantiating PrismaClient in Next.js here: https://www.prisma.io/docs/data-platform/accelerate/getting-started
 
 const prismaClientSingleton = () => {
-  return new PrismaClient().$extends(withAccelerate());
+  return new PrismaClient({
+    accelerateUrl: process.env.DATABASE_URL,
+  })
 };
 
 declare const globalThis: {
--- accelerate/nextjs-starter/prisma/seed.ts
+++ accelerate/nextjs-starter/prisma/seed.ts
@@ -1,6 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL,
+});
 
 const main = async () => {
   console.time("Seeding complete 🌱");
