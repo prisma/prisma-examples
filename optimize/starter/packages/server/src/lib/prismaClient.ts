fdce825d208068805c34897a430b5b2b43f19c98
Aman Varshney <amanvarshney.work@gmail.com>
2025-11-12 02:25:21 +0530

    update examples

--- optimize/starter/packages/server/src/lib/prismaClient.ts
+++ optimize/starter/packages/server/src/lib/prismaClient.ts
@@ -1,11 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/generated/client";
 import { withOptimize } from "@prisma/extension-optimize";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient().$extends(
+  new PrismaClient({
+    adapter,
+  }).$extends(
     withOptimize({
       apiKey: process.env.PRISMA_OPTIMIZE_TOKEN!,
     })
