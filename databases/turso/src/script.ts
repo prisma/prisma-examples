0ceb3f485b677b24f83238f6d4a0912d35f9b6c7
Aman Varshney <amanvarshney.work@gmail.com>
2025-11-18 18:32:57 +0530

    update turso example

--- databases/turso/src/script.ts
+++ databases/turso/src/script.ts
@@ -1,15 +1,11 @@
 require('dotenv').config()
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
-import { createClient } from '@libsql/client'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 
-
-const libsql = createClient({
+const adapter = new PrismaLibSql({
   url: `${process.env.TURSO_DATABASE_URL}`,
   authToken: `${process.env.TURSO_AUTH_TOKEN}`,
 })
-
-const adapter = new PrismaLibSQL(libsql)
 const prisma = new PrismaClient({ adapter })
 
 async function main() {
@@ -24,7 +20,7 @@ async function main() {
   console.log(user)
 
   // find user
-	const foundUser = await prisma.user.findUnique({
+  const foundUser = await prisma.user.findUnique({
     where: {
       email: 'hana@hana.io'
     }
