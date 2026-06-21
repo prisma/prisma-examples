fe87398c9af9ee3f194adad2021f8d42ec0cb8d3
vini <vinidemoraes05@gmail.com>
2026-06-21 14:47:20 -0300

    update path to gerenate at root

--- orm/nextjs/lib/prisma.ts
+++ orm/nextjs/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../prisma/generated/client'
+import { PrismaClient } from '../generated/prisma/client'
 
 const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
 const prisma = new PrismaClient({ adapter: pool })
